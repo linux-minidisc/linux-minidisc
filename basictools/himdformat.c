622160abe6a04c04d59fa69a0b78e126c23d5d32
Michael Karcher <karcher@physik.fu-berlin.de>
2009-04-20 21:26:53 +0200

    Add forgotten file

---
+++ basictools/himdformat.c
@@ -0,0 +1,80 @@
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/ioctl.h>
+#include <scsi/sg.h>
+#include <string.h>
+
+#include <stdio.h>
+
+int main(int argc, char ** argv)
+{
+    int fd;
+    int res;
+    int i;
+    struct sg_io_hdr sg;
+    unsigned char command[12];
+    unsigned char reply[200];
+    unsigned char sense[16];
+    
+    if(argc < 2)
+    {
+        fputs("Please specify the path to the sg device\n",stderr);
+        return 1;
+    }
+    
+    fd = open(argv[1],O_RDWR);
+    if(fd < 0)
+    {
+        perror("Cannot open device");
+        return 1;
+    }
+    if(ioctl(fd,SG_GET_NUM_WAITING,&i) < 0)
+    {
+        perror("ioctl SG_GET_ACCESS_COUNT failed. Is it really sg? Error");
+        return 1;
+    }
+    
+    memset(command,0,12);
+    command[0] = 0xC2;	/* sony special commands */
+    command[4] = 3;     /* subcommand: format */
+    
+    sg.interface_id = 'S';
+    sg.dxfer_direction = SG_DXFER_NONE;
+    sg.cmd_len = 12;
+    sg.mx_sb_len = 16;
+    sg.iovec_count = 0;
+    sg.dxfer_len = 0;
+    sg.dxferp = reply;
+    sg.cmdp = command;
+    sg.sbp = sense;
+    sg.timeout = 10000000;
+    sg.flags = 0;
+    sg.pack_id = 0;
+    sg.usr_ptr = NULL;
+    
+    res = write(fd,&sg,sizeof sg);
+    if(res < 0)
+    {
+        perror("sending SCSI command");
+        return 1;
+    }
+    
+    res = read(fd,&sg,sizeof sg);
+    if(res < 0)
+    {
+        perror("receiving SCSI answer");
+        return 1;
+    }
+
+    if(sg.sb_len_wr)
+    {
+        printf("Formatting failed! Sense data: ");
+        for(i = 0;i < sg.sb_len_wr;i++)
+            printf("%02X ",(unsigned char)sense[i]);
+        putchar('\n');
+        return 1;
+    }
+
+    close(fd);
+    return 0;
+}
