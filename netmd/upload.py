96d716ca974a90a58918c3729b73f6b4d07b7515
Michael Karcher <karcher@physik.fu-berlin.de>
2009-09-09 00:35:26 +0200

    Add upload capabilities for LP2 and LP4
    tested with ffmpeg - playback works great.

--- netmd/upload.py
+++ netmd/upload.py
@@ -5,6 +5,40 @@ import libnetmd
 from time import sleep
 from struct import pack
 
+RIFF_FORMAT_TAG_ATRAC3 = 0x270
+
+UPLOAD_FORMAT_LP4 = 0x80
+UPLOAD_FORMAT_LP2 = 0x82
+UPLOAD_FORMAT_SP = 0x86
+
+# This creates an ffmpeg compatible WAV header.
+class wavUploadEvents(libnetmd.defaultUploadEvents):
+    def __init__(self, stream, channels):
+        self.stream = stream
+        self.channels = channels
+    
+    def trackinfo(self, frames, bytes, format):
+        print 'Format byte', format
+        if format == UPLOAD_FORMAT_LP4:
+            bytesperframe = 96
+            jointstereo = 1
+        else:
+            bytesperframe = 192
+            jointstereo = 0
+        bytespersecond = bytesperframe * 512 / 44100
+        # RIFF header
+        self.stream.write(pack("<4sI4s", 'RIFF', bytes+60, 'WAVE'))
+        # standard fmt chunk
+        self.stream.write(pack("<4sIHHIIHH", 'fmt ',32, RIFF_FORMAT_TAG_ATRAC3,
+                          self.channels, 44100, bytespersecond, 
+                          self.channels * bytesperframe, 0))
+        # ATRAC specific extra data
+        self.stream.write(pack("<HHIHHHH", 14, 1, bytesperframe,
+                                           jointstereo, jointstereo, 1, 0))
+        # data chunk header
+        self.stream.write(pack("<4sI", 'data', bytes))
+        libnetmd.defaultUploadEvents.trackinfo(self, frames, bytes, format)
+
 def main(bus=None, device_address=None, track_range=None):
     context = usb1.LibUSBContext()
     for md in libnetmd.iterdevices(context, bus=bus,
@@ -35,12 +69,12 @@ def getTrackList(md_iface, track_range):
     for track in track_list:
         flags = md_iface.getTrackFlags(track)
         codec, channel_count = md_iface.getTrackEncoding(track)
-        if flags != libnetmd.TRACK_FLAG_PROTECTED and  codec == libnetmd.ENCODING_SP:
+        if flags != libnetmd.TRACK_FLAG_PROTECTED:
             channel_count = libnetmd.CHANNEL_COUNT_DICT[channel_count]
             ascii_title = md_iface.getTrackTitle(track)
             wchar_title = md_iface.getTrackTitle(track, True).decode('shift_jis')
             title = wchar_title or ascii_title
-            append((track,
+            append((track, codec,
                     channel_count,
                     title))
     return result
@@ -67,17 +101,24 @@ def MDDump(md_iface, track_range):
         directory = '.'
     else:
         directory = disc_title;
-    print 'Storing in ', directory
+    print 'Storing in', directory
     if not os.path.exists(directory):
         os.mkdir(directory)
-    for track, channels, title in \
+    for track, codec, channels, title in \
         getTrackList(md_iface, track_range):
 
-        filename = '%02i - %s.aea' % (track + 1, title)
+        if codec == libnetmd.ENCODING_SP:
+            extension = 'aea'
+        else:
+            extension = 'wav'
+        filename = '%02i - %s.%s' % (track + 1, title, extension)
         print 'Uploading', filename
         aeafile = open(filename,"w")
-        aeafile.write(formatAeaHeader(channels=channels))
-        md_iface.saveTrackToStream(track, aeafile)
+        if codec == libnetmd.ENCODING_SP:
+            aeafile.write(formatAeaHeader(channels=channels))
+            md_iface.saveTrackToStream(track, aeafile)
+        else:
+            md_iface.saveTrackToStream(track, aeafile,events=wavUploadEvents(aeafile, channels))
 
     # TODO: generate playlists based on groups defined on the MD
     print 'Finished.'
