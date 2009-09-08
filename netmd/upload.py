50031247b03fb21528d074979e96a5fc83613424
Michael Karcher <karcher@physik.fu-berlin.de>
2009-09-08 21:03:04 +0200

    Add upload.py, a script like dump_md using digital upload for unprotected tracks

---
+++ netmd/upload.py
@@ -0,0 +1,107 @@
+#!/usr/bin/python
+import os
+import usb1
+import libnetmd
+from time import sleep
+from struct import pack
+
+def main(bus=None, device_address=None, track_range=None):
+    context = usb1.LibUSBContext()
+    for md in libnetmd.iterdevices(context, bus=bus,
+                                   device_address=device_address):
+        md_iface = libnetmd.NetMDInterface(md)
+        MDDump(md_iface, track_range)
+
+def getTrackList(md_iface, track_range):
+    result = []
+    append = result.append
+    track_count = md_iface.getTrackCount()
+    if isinstance(track_range, tuple):
+        min_track, max_track = track_range
+        if max_track is None:
+            max_track = track_count - 1
+        assert max_track < track_count
+        assert min_track < track_count
+        track_list = xrange(min_track, max_track + 1)
+    elif isinstance(track_range, int):
+        assert track_range < track_count
+        track_list = [track_range]
+    else:
+        track_list = xrange(track_count)
+    for track in track_list:
+        flags = md_iface.getTrackFlags(track)
+        codec, channel_count = md_iface.getTrackEncoding(track)
+        if flags != libnetmd.TRACK_FLAG_PROTECTED and  codec == libnetmd.ENCODING_SP:
+            channel_count = libnetmd.CHANNEL_COUNT_DICT[channel_count]
+            ascii_title = md_iface.getTrackTitle(track)
+            wchar_title = md_iface.getTrackTitle(track, True).decode('shift_jis')
+            title = wchar_title or ascii_title
+            append((track,
+                    str(channel_count),
+                    title))
+    return result
+
+def formatAeaHeader(name = '', channels = 2, soundgroups = 1, groupstart = 0, encrypted = 0, flags='\0\0\0\0\0\0\0\0'):
+    return pack("2048s", # pad to header size
+                pack("<I256si" #"Bx8sIbI"
+                                      ,2048,   # header size
+                                      name,
+                                      soundgroups,
+                                      #channels,
+                                      #flags,
+                                      #0,      # Should be time of recordin in
+                                              # 32 bit DOS format.
+                                      #encrypted,
+                                      #groupstart
+                                      ))
+
+def MDDump(md_iface, track_range):
+    ascii_title = md_iface.getDiscTitle()
+    wchar_title = md_iface.getDiscTitle(True).decode('shift_jis')
+    disc_title = wchar_title or ascii_title
+    if disc_title == '':
+        directory = '.'
+    else:
+        directory = disc_title;
+    print 'Storing in ', directory
+    if not os.path.exists(directory):
+        os.mkdir(directory)
+    for track, channels, title in \
+        getTrackList(md_iface, track_range):
+
+        filename = '%02i - %s.aea' % (track + 1, title)
+        print 'Uploading', filename
+        aeafile = open(filename,"w")
+        aeafile.write(formatAeaHeader(channels=channels))
+        md_iface.saveTrackToStream(track, aeafile)
+
+    # TODO: generate playlists based on groups defined on the MD
+    print 'Finished.'
+
+if __name__ == '__main__':
+    from optparse import OptionParser
+    parser = OptionParser()
+    parser.add_option('-b', '--bus')
+    parser.add_option('-d', '--device')
+    parser.add_option('-t', '--track-range')
+    (options, args) = parser.parse_args()
+    assert len(args) < 2
+    track_range = options.track_range
+    if track_range is not None:
+        if '-' in track_range:
+            begin, end = track_range.split('-', 1)
+            if begin == '':
+                begin = 0
+            else:
+                begin = int(begin) - 1
+            if end == '':
+                end = None
+            else:
+                end = int(end) - 1
+                assert begin < end
+            track_range = (begin, end)
+        else:
+            track_range = int(track_range) - 1
+    main(bus=options.bus, device_address=options.device,
+         track_range=track_range)
+
