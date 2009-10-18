5d36f4de7e559e01de9be9c19c2d0a2ec3f70676
Mårten Cassel <marten.cassel@gmail.com>
2009-10-18 23:42:23 +0200

    Added Swedish translation for qhimdtransfer.

---
+++ qhimdtransfer/qhimdtransfer_se.ts
@@ -0,0 +1,282 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="sv_SE">
+<context>
+    <name>QHiMDAboutDialog</name>
+    <message>
+        <source>About QHiMDTransfer</source>
+        <translation>Om QHiMDTransfer</translation>
+    </message>
+    <message>
+        <source>&lt;h1&gt;About QHiMDTransfer&lt;/h1&gt;</source>
+        <translation>&lt;h1&gt;Om QHiMDTransfer &lt;/h1&gt;</translation>
+    </message>
+    <message>
+        <source>&lt;h3&gt;QHiMDTransfer, Version 0.0.1&lt;/h3&gt;
+        &lt;p&gt;&lt;p&gt;
+        &lt;p&gt;A free music transfer software for MiniDisc walkman.&lt;/p&gt;
+        &lt;p&gt;&lt;p&gt;
+        &lt;p&gt;Brought to you by the linux-minidisc project:&lt;/p&gt;
+        &lt;p&gt;&lt;p&gt;
+        &lt;p&gt;https://wiki.physik.fu-berlin.de/linux-minidisc&lt;/p&gt;
+        &lt;p&gt;&lt;p&gt;
+        &lt;p&gt;This software is covered by the GNU GPL v2 license. See the
+         file LICENSE in the program folder for more information.&lt;/p&gt;</source>
+        <translation>&lt;h3&gt;QHiMDTransfer, Version 0.0.1&lt;/h3&gt;
+        &lt;p&gt;&lt;p&gt;
+        &lt;p&gt;En fri överförings mjukvara för musik till MiniDisc walkman.&lt;/p&gt;
+        &lt;p&gt;&lt;p&gt;
+        &lt;p&gt;Gjordes möjligt tack vare  linux-minidisc projektet:&lt;/p&gt;
+        &lt;p&gt;&lt;p&gt;
+        &lt;p&gt;https://wiki.physik.fu-berlin.de/linux-minidisc&lt;/p&gt;
+        &lt;p&gt;&lt;p&gt;
+        &lt;p&gt;This software is covered by the GNU GPL v2 license. See the
+         file LICENSE in the program folder for more information.&lt;/p&gt;</translation>
+    </message>
+    <message>
+        <source>&amp;Close</source>
+        <translation>&amp;Stäng</translation>
+    </message>
+</context>
+<context>
+    <name>QHiMDFormatDialog</name>
+    <message>
+        <source>Format Medium</source>
+        <translation>Formatera Media</translation>
+    </message>
+    <message>
+        <source>Really format MiniDisc (all audio-tracks and data will be lost) ?</source>
+        <translation>Vill du verkligen formatera MiniDisken (Alla musikspår och data kommer att försvinna) ?</translation>
+    </message>
+</context>
+<context>
+    <name>QHiMDMainWindow</name>
+    <message>
+        <source>Error opening file for MP3 output</source>
+        <translation>Fel: Går inte att öppna filen för MP3 utdata</translation>
+    </message>
+    <message>
+        <source>Error opening track: </source>
+        <translation>Fel: Går inte att öppna spåret:</translation>
+    </message>
+    <message>
+        <source>Error writing audio data</source>
+        <translation>Fel: Kan inte skriva ljud data</translation>
+    </message>
+    <message>
+        <source>upload aborted by the user</source>
+        <translation>uppladdningen stoppades av användaren</translation>
+    </message>
+    <message>
+        <source>Error reading audio data: </source>
+        <translation>Fel: kunde inte läsa ljud data: </translation>
+    </message>
+    <message>
+        <source>Error opening file for ATRAC output</source>
+        <translation>Fel: kan inte öpppna fil för ATRAC utdata</translation>
+    </message>
+    <message>
+        <source>Error writing header</source>
+        <translation>Fel: kan inte skriva huvud</translation>
+    </message>
+    <message>
+        <source>Error opening file for WAV output</source>
+        <translation>Fel: Kan inte öppna fil för WAV utdata</translation>
+    </message>
+    <message>
+        <source>Select MP3s for download</source>
+        <translation>Välj MP3s för nedladdning</translation>
+    </message>
+    <message>
+        <source>Select directory for Upload</source>
+        <translation>Välj en katalog för Uppladdning</translation>
+    </message>
+    <message>
+        <source>Track %1</source>
+        <translation>Spår %1</translation>
+    </message>
+    <message>
+        <source>upload disabled because of DRM encryption</source>
+        <translation>uppladdning avstängt pga DRM kryptering</translation>
+    </message>
+    <message>
+        <source>Select directory of HiMD Medium</source>
+        <translation>Välj katalog för HiMD Medium</translation>
+    </message>
+    <message>
+        <source>Error opening HiMD data. Make sure you chose the proper root directory of your HiMD-Walkman.
+</source>
+        <translation>Fel när HiMD data skulle öppnas. Kontrollera att du valde korrekt rot katalog för din HiMD-Walkman.
+</translation>
+    </message>
+</context>
+<context>
+    <name>QHiMDMainWindowClass</name>
+    <message>
+        <source>QHiMDTransfer</source>
+        <translation>QHiMDTransfer</translation>
+    </message>
+    <message>
+        <source>&amp;File</source>
+        <translation>&amp;Fil</translation>
+    </message>
+    <message>
+        <source>&amp;?</source>
+        <translation>&amp;?</translation>
+    </message>
+    <message>
+        <source>&amp;Download tracks onto MD</source>
+        <translation>&amp;Ladda ner spår till MD</translation>
+    </message>
+    <message>
+        <source>&amp;Upload tracks from MD</source>
+        <translation>&amp;Ladda upp spår från MD</translation>
+    </message>
+    <message>
+        <source>Re&amp;name</source>
+        <translation>&amp;Bytnamn</translation>
+    </message>
+    <message>
+        <source>Rename selected</source>
+        <translation>Bytnamn vald</translation>
+    </message>
+    <message>
+        <source>D&amp;elete selected</source>
+        <translation>&amp;Radera vald</translation>
+    </message>
+    <message>
+        <source>&amp;Help</source>
+        <translation>&amp;Hjälp</translation>
+    </message>
+    <message>
+        <source>&amp;About</source>
+        <translation>&amp;Om</translation>
+    </message>
+    <message>
+        <source>&amp;Format MD</source>
+        <translation>&amp;Formatera MD</translation>
+    </message>
+    <message>
+        <source>Format MD</source>
+        <translation>Formatera MD</translation>
+    </message>
+    <message>
+        <source>&amp;Quit</source>
+        <translation>&amp;Avsluta</translation>
+    </message>
+    <message>
+        <source>&amp;Connect</source>
+        <translation>&amp;Anslut</translation>
+    </message>
+</context>
+<context>
+    <name>QHiMDTracksModel</name>
+    <message>
+        <source>Nr.</source>
+        <translation>Nr.</translation>
+    </message>
+    <message>
+        <source>Title</source>
+        <translation>Titel</translation>
+    </message>
+    <message>
+        <source>Artist</source>
+        <translation>Artist</translation>
+    </message>
+    <message>
+        <source>Album</source>
+        <translation>Album</translation>
+    </message>
+    <message>
+        <source>Length</source>
+        <translation>Längd</translation>
+    </message>
+    <message>
+        <source>Format</source>
+        <translation>Format</translation>
+    </message>
+    <message>
+        <source>Uploadable</source>
+        <translation>Uppladdningsbar</translation>
+    </message>
+</context>
+<context>
+    <name>QHiMDUploadDialog</name>
+    <message>
+        <source>%1 track(s) could not be uploaded</source>
+        <translation>%1 spår kunde inte laddas upp</translation>
+    </message>
+    <message>
+        <source>Track %1</source>
+        <translation>Spår %1</translation>
+    </message>
+    <message>
+        <source>%1 track(s) successfully uploaded</source>
+        <translation>%1 spår laddades upp</translation>
+    </message>
+    <message>
+        <source>upload finished</source>
+        <translation>uppladdningen avslutad</translation>
+    </message>
+    <message>
+        <source>current track: %1 - %2</source>
+        <translation>nuvarande spår: %1 - %2</translation>
+    </message>
+    <message>
+        <source>no tracks selected</source>
+        <translation>inget spår valt</translation>
+    </message>
+    <message>
+        <source>please wait while uploading %1 track(s)</source>
+        <translation>var god vänta medan %1 spår laddas upp</translation>
+    </message>
+    <message>
+        <source>upload aborted by the user</source>
+        <translation>uppladdningen avbruten av användaren</translation>
+    </message>
+    <message>
+        <source>Track upload - QHiMDTransfer</source>
+        <translation>Spår uppladdning - QHiMDTransfer</translation>
+    </message>
+    <message>
+        <source>please wait while uploading XX track(s)</source>
+        <translation>var god vänta medan uppladdning XX spår</translation>
+    </message>
+    <message>
+        <source>overall progress</source>
+        <translation>fortlöpande process</translation>
+    </message>
+    <message>
+        <source>current track: trknum - filename</source>
+        <translation>nuvarande spår: trknum - filnamn</translation>
+    </message>
+    <message>
+        <source>xx track(s) successfully uploaded</source>
+        <translation>xx spår laddades upp</translation>
+    </message>
+    <message>
+        <source>xx track(s) could not be uploaded</source>
+        <translation>xx spår kunde inte laddas upp</translation>
+    </message>
+    <message>
+        <source>Show Details</source>
+        <translation>Visa Detaljer</translation>
+    </message>
+    <message>
+        <source>Cancel</source>
+        <translation>Avbryt</translation>
+    </message>
+    <message>
+        <source>Close</source>
+        <translation>Stäng</translation>
+    </message>
+    <message>
+        <source>Tracknr.</source>
+        <translation>Spårnr.</translation>
+    </message>
+    <message>
+        <source>Error message</source>
+        <translation>Felmeddelande</translation>
+    </message>
+</context>
+</TS>
