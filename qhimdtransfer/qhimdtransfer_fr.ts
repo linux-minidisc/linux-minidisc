ddf5352d19040e9f2f988da7d2477576215dd073
Daniel Berthereau <daniel@berthereau.net>
2009-10-28 21:02:16 +0100

    added French translation for qhimdtransfer.

---
+++ qhimdtransfer/qhimdtransfer_fr.ts
@@ -0,0 +1,280 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="fr_FR">
+<context>
+    <name>QHiMDAboutDialog</name>
+    <message>
+        <source>About QHiMDTransfer</source>
+        <translation>À propos de QHiMDTransfer</translation>
+    </message>
+    <message>
+        <source>&lt;h1&gt;About QHiMDTransfer&lt;/h1&gt;</source>
+        <translation>&lt;h1&gt;À propos de QHiMDTransfer&lt;/h1&gt;</translation>
+    </message>
+    <message>
+        <source>&lt;h3&gt;QHiMDTransfer, Version 0.0.1&lt;/h3&gt;
+        &lt;p&gt;&lt;p&gt;
+        &lt;p&gt;A free music transfer software for MiniDisc walkman.&lt;/p&gt;
+        &lt;p&gt;&lt;p&gt;
+        &lt;p&gt;Brought to you by the linux-minidisc project:&lt;/p&gt;
+        &lt;p&gt;&lt;p&gt;
+        &lt;p&gt;https://wiki.physik.fu-berlin.de/linux-minidisc&lt;/p&gt;
+        &lt;p&gt;&lt;p&gt;
+        &lt;p&gt;This software is covered by the GNU GPL v2 license. See the
+         file LICENSE in the program folder for more information.&lt;/p&gt;</source>
+        <translation>&lt;h3&gt; QHiMDTransfer, Version 0.0.1 &lt;/h3&gt;
+         &lt;p&gt;&lt;p&gt;
+         &lt;p&gt;Un logiciel libre pour transférer la musique d&apos;un Walkman MiniDisc.&lt;/p&gt;
+         &lt;p&gt;&lt;p&gt;
+         &lt;p&gt;Conçu pour vous par le projet Linux-minidisc:&lt;/p&gt;
+         &lt;p&gt;&lt;p&gt;
+         &lt;p&gt;https://wiki.physik.fu-berlin.de/linux-minidisc&lt;/p&gt;
+         &lt;p&gt;&lt;p&gt;
+         &lt;p&gt;Ce logiciel est couvert par la licence GNU GPL v2. Voir la fichier de licence dans le dossier du programme pour plus d&apos;informations.&lt;/p&gt;</translation>
+    </message>
+    <message>
+        <source>&amp;Close</source>
+        <translation>&amp;Fermer</translation>
+    </message>
+</context>
+<context>
+    <name>QHiMDFormatDialog</name>
+    <message>
+        <source>Format Medium</source>
+        <translation>Formater le MiniDisc</translation>
+    </message>
+    <message>
+        <source>Really format MiniDisc (all audio-tracks and data will be lost) ?</source>
+        <translation>Voulez-vous vraiment formater le MiniDisc (toutes les pistes audio et les données seront perdues) ?</translation>
+    </message>
+</context>
+<context>
+    <name>QHiMDMainWindow</name>
+    <message>
+        <source>Track %1</source>
+        <translation>Piste %1</translation>
+    </message>
+    <message>
+        <source>upload disabled because of DRM encryption</source>
+        <translation>Importation désactivée en raison des protections DRM</translation>
+    </message>
+    <message>
+        <source>Error opening file for MP3 output</source>
+        <translation>Erreur d&apos;ouverture de fichier pour l&apos;export en MP3</translation>
+    </message>
+    <message>
+        <source>Error opening track: </source>
+        <translation>Erreur lors de l&apos;ouverture des pistes : </translation>
+    </message>
+    <message>
+        <source>Error writing audio data</source>
+        <translation>Erreur lors de l&apos;écriture des données audio</translation>
+    </message>
+    <message>
+        <source>upload aborted by the user</source>
+        <translation>Importation annulée par l&apos;utilisateur</translation>
+    </message>
+    <message>
+        <source>Error reading audio data: </source>
+        <translation>Erreur lors de la lecture des données audio : </translation>
+    </message>
+    <message>
+        <source>Error opening file for ATRAC output</source>
+        <translation>Erreur d&apos;ouverture de fichier pour l&apos;export en ATRAC</translation>
+    </message>
+    <message>
+        <source>Error writing header</source>
+        <translation>Erreur lors de l&apos;écriture des données d&apos;entête</translation>
+    </message>
+    <message>
+        <source>Error opening file for WAV output</source>
+        <translation>Erreur d&apos;ouverture de fichier pour l&apos;export en WAV</translation>
+    </message>
+    <message>
+        <source>Select MP3s for download</source>
+        <translation>Sélectionnez les MP3 à exporter</translation>
+    </message>
+    <message>
+        <source>Select directory for Upload</source>
+        <translation>Choisissez un dossier pour l&apos;importation</translation>
+    </message>
+    <message>
+        <source>Select directory of HiMD Medium</source>
+        <translation>Choisissez un dossier sur le disque HiMD</translation>
+    </message>
+    <message>
+        <source>Error opening HiMD data. Make sure you chose the proper root directory of your HiMD-Walkman.
+</source>
+        <translation>Erreur lors de l&apos;ouverture des données HiMD. Assurez-vous d&apos;avoir choisi le dossier racine approprié sur votre Walkman HiMD.</translation>
+    </message>
+</context>
+<context>
+    <name>QHiMDMainWindowClass</name>
+    <message>
+        <source>QHiMDTransfer</source>
+        <translation>QHiMDTransfer</translation>
+    </message>
+    <message>
+        <source>&amp;File</source>
+        <translation>&amp;Fichier</translation>
+    </message>
+    <message>
+        <source>&amp;?</source>
+        <translation>&amp;?</translation>
+    </message>
+    <message>
+        <source>&amp;Download tracks onto MD</source>
+        <translation>E&amp;xporter des pistes vers le MD</translation>
+    </message>
+    <message>
+        <source>&amp;Upload tracks from MD</source>
+        <translation>&amp;Importer des pistes depuis le MD</translation>
+    </message>
+    <message>
+        <source>Re&amp;name</source>
+        <translation>&amp;Renommer</translation>
+    </message>
+    <message>
+        <source>Rename selected</source>
+        <translation>Renommer la sélection</translation>
+    </message>
+    <message>
+        <source>D&amp;elete selected</source>
+        <translation>&amp;Supprimer la sélection</translation>
+    </message>
+    <message>
+        <source>&amp;Help</source>
+        <translation>&amp;Aide</translation>
+    </message>
+    <message>
+        <source>&amp;About</source>
+        <translation>À &amp;propos</translation>
+    </message>
+    <message>
+        <source>&amp;Format MD</source>
+        <translation>&amp;Formater le MD</translation>
+    </message>
+    <message>
+        <source>Format MD</source>
+        <translation>Formater le MD</translation>
+    </message>
+    <message>
+        <source>&amp;Quit</source>
+        <translation>&amp;Quitter</translation>
+    </message>
+    <message>
+        <source>&amp;Connect</source>
+        <translation>&amp;Connecter</translation>
+    </message>
+</context>
+<context>
+    <name>QHiMDTracksModel</name>
+    <message>
+        <source>Title</source>
+        <translation>Titre</translation>
+    </message>
+    <message>
+        <source>Artist</source>
+        <translation>Artiste</translation>
+    </message>
+    <message>
+        <source>Album</source>
+        <translation>Album</translation>
+    </message>
+    <message>
+        <source>Length</source>
+        <translation>Durée</translation>
+    </message>
+    <message>
+        <source>Uploadable</source>
+        <translation>Importable</translation>
+    </message>
+    <message>
+        <source>Format</source>
+        <translation>Format</translation>
+    </message>
+    <message>
+        <source>Nr.</source>
+        <translation>N°</translation>
+    </message>
+</context>
+<context>
+    <name>QHiMDUploadDialog</name>
+    <message>
+        <source>upload finished</source>
+        <translation>Importation terminée</translation>
+    </message>
+    <message>
+        <source>%1 track(s) successfully uploaded</source>
+        <translation>%1 piste(s) importée(s) avec succès</translation>
+    </message>
+    <message>
+        <source>%1 track(s) could not be uploaded</source>
+        <translation>%1 piste(s) ne peuvent pas être importées</translation>
+    </message>
+    <message>
+        <source>Track %1</source>
+        <translation>Piste %1</translation>
+    </message>
+    <message>
+        <source>current track: %1 - %2</source>
+        <translation>Piste actuelle : %1 - %2</translation>
+    </message>
+    <message>
+        <source>no tracks selected</source>
+        <translation>Aucune piste sélectionnée</translation>
+    </message>
+    <message>
+        <source>please wait while uploading %1 track(s)</source>
+        <translation>Merci de patienter pendant l&apos;importation de %1 piste(s)</translation>
+    </message>
+    <message>
+        <source>Track upload - QHiMDTransfer</source>
+        <translation>Importation de piste - QHiMDTransfer</translation>
+    </message>
+    <message>
+        <source>please wait while uploading XX track(s)</source>
+        <translation>Merci de patentier pendant l&apos;importation de XX piste(s)</translation>
+    </message>
+    <message>
+        <source>overall progress</source>
+        <translation>Progression des opérations</translation>
+    </message>
+    <message>
+        <source>current track: trknum - filename</source>
+        <translation>Piste actuelle : trknum - filename</translation>
+    </message>
+    <message>
+        <source>xx track(s) successfully uploaded</source>
+        <translation>xx piste(s) importée(s) avec succès</translation>
+    </message>
+    <message>
+        <source>xx track(s) could not be uploaded</source>
+        <translation>xx piste(s) ne peuvent pas être importées</translation>
+    </message>
+    <message>
+        <source>Show Details</source>
+        <translation>Afficher les détails</translation>
+    </message>
+    <message>
+        <source>Cancel</source>
+        <translation>Annuler</translation>
+    </message>
+    <message>
+        <source>Close</source>
+        <translation>Fermer</translation>
+    </message>
+    <message>
+        <source>Tracknr.</source>
+        <translation>N° piste</translation>
+    </message>
+    <message>
+        <source>Error message</source>
+        <translation>Message d&apos;erreur</translation>
+    </message>
+    <message>
+        <source>upload aborted by the user</source>
+        <translation>Importation abandonnée par l&apos;utilisateur</translation>
+    </message>
+</context>
+</TS>
