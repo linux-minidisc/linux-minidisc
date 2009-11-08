749fab1516195336058af4ad9d65d3436f7e4a84
Rui Principe <ruiprincipe@googlemail.com>
2009-11-08 23:23:10 +0100

    Added Portuguese translation.

---
+++ qhimdtransfer/qhimdtransfer_pt.ts
@@ -0,0 +1,306 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="pt_PT">
+<context>
+    <name>QHiMDAboutDialog</name>
+    <message>
+        <source>About QHiMDTransfer</source>
+        <translation>Sobre o QHiMDTransfer</translation>
+    </message>
+    <message>
+        <source>&lt;h1&gt;About QHiMDTransfer&lt;/h1&gt;</source>
+        <translation>&lt;h1&gt;Sobre o QHiMDTransfer&lt;/h1&gt;</translation>
+    </message>
+    <message>
+        <source>&lt;h3&gt;QHiMDTransfer, Version 0.0.1&lt;/h3&gt;
+        &lt;p&gt;&lt;p&gt;
+        &lt;p&gt;A free music transfer software for MiniDisc walkman.&lt;/p&gt;
+        &lt;p&gt;&lt;p&gt;
+        &lt;p&gt;Brought to you by the linux-minidisc project:&lt;/p&gt;
+        &lt;p&gt;&lt;p&gt;
+        &lt;p&gt;https://wiki.physik.fu-berlin.de/linux-minidisc&lt;/p&gt;
+        &lt;p&gt;&lt;p&gt;
+        &lt;p&gt;This software is covered by the GNU GPL v2 license. See the
+         file LICENSE in the program folder for more information.&lt;/p&gt;</source>
+        <translation>&lt;h3&gt;QHiMDTransfer, Versão 0.0.1&lt;/h3&gt;
+        &lt;p&gt;&lt;p&gt;
+        &lt;p&gt;Um software gratuito de transferência de música para MiniDisc walkman.&lt;/p&gt;
+        &lt;p&gt;&lt;p&gt;
+        &lt;p&gt;Disponibilizado pelo projecto linux-minidisc:&lt;/p&gt;
+        &lt;p&gt;&lt;p&gt;
+        &lt;p&gt;https://wiki.physik.fu-berlin.de/linux-minidisc&lt;/p&gt;
+        &lt;p&gt;&lt;p&gt;
+        &lt;p&gt;Este software está coberto pela licença GNU GPL v2. Veja o
+         ficheiro LICENSE na pasta do programa para mais informação.&lt;/p&gt;</translation>
+    </message>
+    <message>
+        <source>&amp;Close</source>
+        <translation>&amp;Fechar</translation>
+    </message>
+</context>
+<context>
+    <name>QHiMDFormatDialog</name>
+    <message>
+        <source>Format Medium</source>
+        <translation>Formatar Meio</translation>
+    </message>
+    <message>
+        <source>Really format MiniDisc (all audio-tracks and data will be lost) ?</source>
+        <translation>Quer mesmo formatar o MiniDisc (todas as faixas de aúdio e dados serão perdidas)?</translation>
+    </message>
+</context>
+<context>
+    <name>QHiMDMainWindow</name>
+    <message>
+        <source>Error opening file for MP3 output</source>
+        <translation>Erro ao abrir ficheiro para saída de MP3</translation>
+    </message>
+    <message>
+        <source>Error opening track: </source>
+        <translation>Erro ao abrir a faixa: </translation>
+    </message>
+    <message>
+        <source>Error writing audio data</source>
+        <translation>Erro ao escrever dados de áudio</translation>
+    </message>
+    <message>
+        <source>upload aborted by the user</source>
+        <translation>carregamento abortado pelo utilizador</translation>
+    </message>
+    <message>
+        <source>Error reading audio data: </source>
+        <translation>Erro na leitura dos dados de áudio: </translation>
+    </message>
+    <message>
+        <source>Error opening file for ATRAC output</source>
+        <translation>Erro ao abrir ficheiro para saída de ATRAC</translation>
+    </message>
+    <message>
+        <source>Error writing header</source>
+        <translation>Erro na escrita de header</translation>
+    </message>
+    <message>
+        <source>Error opening file for WAV output</source>
+        <translation>Erro ao abrir ficheiro para saída de WAV</translation>
+    </message>
+    <message>
+        <source>Error opening HiMD data. Make sure you chose the proper root directory of your HiMD-Walkman.
+</source>
+        <translation>Erro ao abrir dados HiMD. Certifique-se  de que escolheu o directório raiz correcto do seu HiMD-Walkman.
+</translation>
+    </message>
+    <message>
+        <source>Track %1</source>
+        <translation>Faixa %1</translation>
+    </message>
+    <message>
+        <source>upload disabled because of DRM encryption</source>
+        <translation>carregamento desactivado por causa de encriptação DRM</translation>
+    </message>
+    <message>
+        <source>Select MP3s for download</source>
+        <translation>Seleccione os MP3 para baixar</translation>
+    </message>
+    <message>
+        <source>Select directory for Upload</source>
+        <translation>Seleccionar o directório para Carregamento</translation>
+    </message>
+    <message>
+        <source>Select directory of HiMD Medium</source>
+        <translation>Seleccionar o directório do Meio HiMD</translation>
+    </message>
+</context>
+<context>
+    <name>QHiMDMainWindowClass</name>
+    <message>
+        <source>QHiMDTransfer</source>
+        <translation>QHiMDTransfer</translation>
+    </message>
+    <message>
+        <source>HiMD path</source>
+        <translation>Caminho do HiMD</translation>
+    </message>
+    <message>
+        <source>(disconnected)</source>
+        <translation>(desconectado)</translation>
+    </message>
+    <message>
+        <source>&gt;&gt;</source>
+        <translation>&gt;&gt;</translation>
+    </message>
+    <message>
+        <source>&lt;&lt;</source>
+        <translation>&lt;&lt;</translation>
+    </message>
+    <message>
+        <source>local directory</source>
+        <translation>directório local</translation>
+    </message>
+    <message>
+        <source>/tmp</source>
+        <translation>/tmp</translation>
+    </message>
+    <message>
+        <source>&amp;File</source>
+        <translation>&amp;Ficheiro</translation>
+    </message>
+    <message>
+        <source>&amp;?</source>
+        <translation>&amp;?</translation>
+    </message>
+    <message>
+        <source>&amp;Download tracks onto MD</source>
+        <translation>&amp;Baixar faixas para o MD</translation>
+    </message>
+    <message>
+        <source>&amp;Upload tracks from MD</source>
+        <translation>&amp;Carregar faixas do MD</translation>
+    </message>
+    <message>
+        <source>Re&amp;name</source>
+        <translation>Re&amp;nomear</translation>
+    </message>
+    <message>
+        <source>Rename selected</source>
+        <translation>Renomear selecção</translation>
+    </message>
+    <message>
+        <source>D&amp;elete selected</source>
+        <translation>&amp;Remover selecção</translation>
+    </message>
+    <message>
+        <source>&amp;Help</source>
+        <translation>&amp;Ajuda</translation>
+    </message>
+    <message>
+        <source>&amp;About</source>
+        <translation>&amp;Sobre</translation>
+    </message>
+    <message>
+        <source>&amp;Format MD</source>
+        <translation>&amp;Formatar MD</translation>
+    </message>
+    <message>
+        <source>Format MD</source>
+        <translation>Formatar MD</translation>
+    </message>
+    <message>
+        <source>&amp;Quit</source>
+        <translation>&amp;Sair</translation>
+    </message>
+    <message>
+        <source>&amp;Connect</source>
+        <translation>&amp;Ligar</translation>
+    </message>
+</context>
+<context>
+    <name>QHiMDTracksModel</name>
+    <message>
+        <source>Nr.</source>
+        <translation>Nr.</translation>
+    </message>
+    <message>
+        <source>Title</source>
+        <translation>Título</translation>
+    </message>
+    <message>
+        <source>Artist</source>
+        <translation>Artista</translation>
+    </message>
+    <message>
+        <source>Album</source>
+        <translation>Álbum</translation>
+    </message>
+    <message>
+        <source>Length</source>
+        <translation>Duração</translation>
+    </message>
+    <message>
+        <source>Format</source>
+        <translation>Formato</translation>
+    </message>
+    <message>
+        <source>Uploadable</source>
+        <translation>Carregável</translation>
+    </message>
+</context>
+<context>
+    <name>QHiMDUploadDialog</name>
+    <message>
+        <source>%1 track(s) could not be uploaded</source>
+        <translation>%1 faixa(s) não poderam ser carregadas</translation>
+    </message>
+    <message>
+        <source>Track %1</source>
+        <translation>Faixa %1</translation>
+    </message>
+    <message>
+        <source>%1 track(s) successfully uploaded</source>
+        <translation>%1 faixa(s) carregada(s) com sucesso</translation>
+    </message>
+    <message>
+        <source>upload finished</source>
+        <translation>carregamento concluído</translation>
+    </message>
+    <message>
+        <source>current track: %1 - %2</source>
+        <translation>faixa actual: %1 - %2</translation>
+    </message>
+    <message>
+        <source>no tracks selected</source>
+        <translation>nenhumas faixas seleccionadas</translation>
+    </message>
+    <message>
+        <source>please wait while uploading %1 track(s)</source>
+        <translation>por favor aguarde enquanto %1 faixa(s) são carregadas</translation>
+    </message>
+    <message>
+        <source>upload aborted by the user</source>
+        <translation>carregamento abortado pelo utilizador</translation>
+    </message>
+    <message>
+        <source>Track upload - QHiMDTransfer</source>
+        <translation>Carregamento de faixas - QHiMDTransfer</translation>
+    </message>
+    <message>
+        <source>please wait while uploading XX track(s)</source>
+        <translation>por favor aguarde enquanto XX faixa(s) são carregadas</translation>
+    </message>
+    <message>
+        <source>overall progress</source>
+        <translation>progresso geral</translation>
+    </message>
+    <message>
+        <source>current track: trknum - filename</source>
+        <translation>faixa actual: trknum - filename</translation>
+    </message>
+    <message>
+        <source>xx track(s) successfully uploaded</source>
+        <translation>xx faixa(s) carregada(s) com sucesso</translation>
+    </message>
+    <message>
+        <source>xx track(s) could not be uploaded</source>
+        <translation>xx faixa(s) não puderam ser carregadas</translation>
+    </message>
+    <message>
+        <source>Show Details</source>
+        <translation>Mostrar Detalhes</translation>
+    </message>
+    <message>
+        <source>Cancel</source>
+        <translation>Cancelar</translation>
+    </message>
+    <message>
+        <source>Close</source>
+        <translation>Fechar</translation>
+    </message>
+    <message>
+        <source>Tracknr.</source>
+        <translation>Faixanr.</translation>
+    </message>
+    <message>
+        <source>Error message</source>
+        <translation>Mensagem de Erro</translation>
+    </message>
+</context>
+</TS>
