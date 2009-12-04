46ea6d7ea7a8e80e9d1378c36c5d0b2976b6d040
Piotr Patoka <patoka@physik.fu-berlin.de>
2009-12-04 13:55:34 +0100

    Updated Polish translation.

--- qhimdtransfer/qhimdtransfer_pl.ts
+++ qhimdtransfer/qhimdtransfer_pl.ts
@@ -1,15 +1,15 @@
 <?xml version="1.0" encoding="utf-8"?>
-<!DOCTYPE TS><TS version="1.1" language="pl">
-<defaultcodec></defaultcodec>
+<!DOCTYPE TS>
+<TS version="2.0" language="pl_PL">
 <context>
     <name>QHiMDAboutDialog</name>
     <message>
         <source>About QHiMDTransfer</source>
-        <translation type="unfinished"></translation>
+        <translation>O...QHiMDTransfer</translation>
     </message>
     <message>
         <source>&lt;h1&gt;About QHiMDTransfer&lt;/h1&gt;</source>
-        <translation type="unfinished"></translation>
+        <translation>O...QHiMDTransfer</translation>
     </message>
     <message>
         <source>&lt;h3&gt;QHiMDTransfer, Version 0.0.1&lt;/h3&gt;
@@ -22,106 +22,109 @@
         &lt;p&gt;&lt;p&gt;
         &lt;p&gt;This software is covered by the GNU GPL v2 license. See the
          file LICENSE in the program folder for more information.&lt;/p&gt;</source>
-        <translation type="unfinished"></translation>
+        <translation>Darmowe oprogramowanie do transferu muzyki dla walkmana MiniDisc
+Dostarczone przez The linux-minidisc project:
+https://wiki.physik.fu-berlin.de/linux-minidisc
+Oprogramowanie to jest objęte przez licencje GNU GPL v2. Przejrzyj plik LICENSE w folderze programu w celu uzyskania więcej informacji.</translation>
     </message>
     <message>
         <source>&amp;Close</source>
-        <translation type="unfinished"></translation>
+        <translation>Zamknij</translation>
     </message>
 </context>
 <context>
     <name>QHiMDFormatDialog</name>
     <message>
         <source>Format Medium</source>
-        <translation type="unfinished">Formatuj NoĹnik</translation>
+        <translation>Formatuj Nośnik</translation>
     </message>
     <message>
         <source>Really format MiniDisc (all audio-tracks and data will be lost) ?</source>
-        <translation type="unfinished">Napewno sformatowaÄ MiniDisc (wszystkie ĹcieĹźki i dane zostanÄ utracone) ?</translation>
+        <translation>Czy napewno sformatować MiniDisc (wszystkie ścieżki i dane zostaną utracone) ?</translation>
     </message>
 </context>
 <context>
     <name>QHiMDMainWindow</name>
     <message>
         <source>Error opening file for MP3 output</source>
-        <translation type="unfinished">BĹÄd przy otwieraniu pliku @@@ FIXME</translation>
+        <translation>Błąd przy otwieraniu pliku dla wyjścia MP3</translation>
     </message>
     <message>
         <source>Error opening track: </source>
-        <translation>BĹÄd otwierania scieĹźki:</translation>
+        <translation>Błąd otwierania ścieżki::</translation>
     </message>
     <message>
         <source>Error writing audio data</source>
-        <translation>BĹÄd przy zapisywaniu danych audio</translation>
+        <translation>Błąd zapisu danych audio</translation>
     </message>
     <message>
         <source>upload aborted by the user</source>
-        <translation type="unfinished">WysyĹanie anulowane przez uĹźytkownika</translation>
+        <translation>Wysyłanie anulowane przez użytkownika</translation>
     </message>
     <message>
         <source>Error reading audio data: </source>
-        <translation type="unfinished">BĹÄd przy odczytywaniu danych audio</translation>
+        <translation>Błąd odczytu danych audio</translation>
     </message>
     <message>
         <source>Error opening file for ATRAC output</source>
-        <translation type="unfinished">BĹÄd przy otwieraniu pliku dla wyjĹcia ATRAC</translation>
+        <translation>Błąd przy otwieraniu pliku dla wyjścia ATRAC</translation>
     </message>
     <message>
         <source>Error writing header</source>
-        <translation type="unfinished">BĹÄd przy zapisywaniu nagĹĂłwka</translation>
+        <translation>Błąd zapisu nagłówka</translation>
     </message>
     <message>
         <source>Error opening file for WAV output</source>
-        <translation type="unfinished">BĹÄd przy otwieraniu pliku dla wyjĹcia WAV</translation>
+        <translation>Błąd przy otwieraniu pliku dla wyjścia WAV</translation>
     </message>
     <message>
         <source>Error opening HiMD data. Make sure you chose the proper root directory of your HiMD-Walkman.
 </source>
-        <translation type="unfinished">BĹÄd otwierania HiMD. Upewnij siÄ Ĺźe wybraĹeĹ prawidĹowy katalog/katalog gĹĂłwny HiMD-Walkman
+        <translation>Błąd otwierania danych HiMD. Upewnij się że wybrałeś prawidłowy katalog główny HiMD-Walkman
 </translation>
     </message>
     <message>
         <source>Track %1</source>
-        <translation>ĹcieĹźka %1</translation>
+        <translation>Ścieżka %1</translation>
     </message>
     <message>
         <source>upload disabled because of DRM encryption</source>
-        <translation type="unfinished">WysyĹanie zablokowane z powodu szyfrowania DRM</translation>
+        <translation>Wysyłanie zablokowane z powodu szyfrowania DRM</translation>
     </message>
     <message>
         <source>Select MP3s for download</source>
-        <translation type="unfinished">Wybierz MP3s do wysĹania (przesĹania?)</translation>
+        <translation>Wybierz pliki MP3 do pobrania</translation>
     </message>
     <message>
         <source>Select directory for Upload</source>
-        <translation type="unfinished"></translation>
+        <translation>Wybierz katalog do Wysyłania</translation>
     </message>
     <message>
         <source>Select directory of HiMD Medium</source>
-        <translation type="unfinished"></translation>
+        <translation>Wybierz katalog nośnika HiMD</translation>
     </message>
 </context>
 <context>
     <name>QHiMDMainWindowClass</name>
     <message>
         <source>QHiMDTransfer</source>
-        <translation type="unfinished"></translation>
+        <translation>QHiMDTransfer</translation>
     </message>
     <message>
         <source>HiMD path</source>
-        <translation type="unfinished"></translation>
+        <translation>ścieżka HiMD</translation>
     </message>
     <message>
         <source>(disconnected)</source>
-        <translation type="unfinished"></translation>
+        <translation>(rozłączony)</translation>
     </message>
     <message>
         <source>&gt;&gt;</source>
-        <translation type="unfinished"></translation>
+        <translation>&gt;&gt;</translation>
     </message>
     <message>
         <source>&lt;&lt;</source>
-        <translation type="unfinished"></translation>
+        <translation>&lt;&lt;</translation>
     </message>
     <message>
         <source>local directory</source>
@@ -129,169 +132,169 @@
     </message>
     <message>
         <source>/tmp</source>
-        <translation type="unfinished"></translation>
+        <translation>/tmp</translation>
     </message>
     <message>
         <source>&amp;File</source>
-        <translation type="unfinished">&amp;Plik</translation>
+        <translation>&amp;Plik</translation>
     </message>
     <message>
         <source>&amp;?</source>
-        <translation type="unfinished"></translation>
+        <translation>&amp;?</translation>
     </message>
     <message>
         <source>&amp;Download tracks onto MD</source>
-        <translation type="unfinished">&amp;Wgraj scieĹźki na MD</translation>
+        <translation>&amp;Pobierz scieżki na MD</translation>
     </message>
     <message>
         <source>&amp;Upload tracks from MD</source>
-        <translation type="unfinished">ĹciÄgnij scieĹźki z MD (&amp;S)</translation>
+        <translation>&amp;Wyślij ścieżki z MD</translation>
     </message>
     <message>
         <source>Re&amp;name</source>
-        <translation type="unfinished">&amp;ZmieĹ nazwÄ</translation>
+        <translation>&amp;Zmień nazwę</translation>
     </message>
     <message>
         <source>Rename selected</source>
-        <translation type="unfinished">ZmieĹ nazwy zaznaczonych</translation>
+        <translation>Zmień nazwy zaznaczonych</translation>
     </message>
     <message>
         <source>D&amp;elete selected</source>
-        <translation type="unfinished">&amp;UsuĹ zaznaczone</translation>
+        <translation>&amp;Usuń zaznaczone</translation>
     </message>
     <message>
         <source>&amp;Help</source>
-        <translation type="unfinished">P&amp;omoc</translation>
+        <translation>&amp;Pomoc</translation>
     </message>
     <message>
         <source>&amp;About</source>
-        <translation type="unfinished"></translation>
+        <translation>&amp;O...</translation>
     </message>
     <message>
         <source>&amp;Format MD</source>
-        <translation type="unfinished">&amp;Formatuj MD</translation>
+        <translation>&amp;Formatuj MD</translation>
     </message>
     <message>
         <source>Format MD</source>
-        <translation type="unfinished">Formatuj MD</translation>
+        <translation>Formatuj MD</translation>
     </message>
     <message>
         <source>&amp;Quit</source>
-        <translation type="unfinished">&amp;ZakoĹcz</translation>
+        <translation>&amp;Zakończ</translation>
     </message>
     <message>
         <source>&amp;Connect</source>
-        <translation type="unfinished">P&amp;oĹÄcz</translation>
+        <translation>&amp;Połącz</translation>
     </message>
 </context>
 <context>
     <name>QHiMDTracksModel</name>
     <message>
         <source>Nr.</source>
-        <translation type="unfinished"></translation>
+        <translation>Nr.</translation>
     </message>
     <message>
         <source>Title</source>
-        <translation type="unfinished">TytuĹ</translation>
+        <translation>Tytuł</translation>
     </message>
     <message>
         <source>Artist</source>
-        <translation type="unfinished">Artysta or Wykonawca (both have similar meaning, for music second might be better but mean Performer)</translation>
+        <translation>Wykonawca</translation>
     </message>
     <message>
         <source>Album</source>
-        <translation type="unfinished">Album</translation>
+        <translation>Album</translation>
     </message>
     <message>
         <source>Length</source>
-        <translation type="unfinished">DĹugoĹÄ</translation>
+        <translation>Czas trwania</translation>
     </message>
     <message>
         <source>Format</source>
-        <translation type="unfinished">Format</translation>
+        <translation>Format</translation>
     </message>
     <message>
         <source>Uploadable</source>
-        <translation type="unfinished">hard to translate to one word, </translation>
+        <translation>Można wysyłać</translation>
     </message>
 </context>
 <context>
     <name>QHiMDUploadDialog</name>
     <message>
         <source>Track upload - QHiMDTransfer</source>
-        <translation type="unfinished"></translation>
+        <translation>Wysyłanie ścieżki - QHiMDTransfer</translation>
     </message>
     <message>
         <source>please wait while uploading XX track(s)</source>
-        <translation type="unfinished"></translation>
+        <translation>Proszę czekać..trwa wysyłanie XX ścieżki (ścieżek)</translation>
     </message>
     <message>
         <source>overall progress</source>
-        <translation type="unfinished"></translation>
+        <translation>Postęp całkowity</translation>
     </message>
     <message>
         <source>current track: trknum - filename</source>
-        <translation type="unfinished"></translation>
+        <translation>bieżąca ścieżka: trknr - nazwa pliku</translation>
     </message>
     <message>
         <source>xx track(s) successfully uploaded</source>
-        <translation type="unfinished"></translation>
+        <translation>xx ścieżek pomyślnie wysłanych</translation>
     </message>
     <message>
         <source>xx track(s) could not be uploaded</source>
-        <translation type="unfinished"></translation>
+        <translation>xx ścieżek nie mogło być wysłanych</translation>
     </message>
     <message>
         <source>Show Details</source>
-        <translation type="unfinished"></translation>
+        <translation>Pokaż Szczegóły</translation>
     </message>
     <message>
         <source>Cancel</source>
-        <translation type="unfinished"></translation>
+        <translation>Przerwij</translation>
     </message>
     <message>
         <source>Close</source>
-        <translation type="unfinished"></translation>
+        <translation>Zamknij</translation>
     </message>
     <message>
         <source>Tracknr.</source>
-        <translation type="unfinished"></translation>
+        <translation>Nr ścieżki</translation>
     </message>
     <message>
         <source>Error message</source>
-        <translation type="unfinished"></translation>
+        <translation>Komunikat o błędzie</translation>
     </message>
     <message>
         <source>%1 track(s) could not be uploaded</source>
-        <translation type="unfinished"></translation>
+        <translation>%1 ścieżki nie mogły być wysłane</translation>
     </message>
     <message>
         <source>Track %1</source>
-        <translation type="unfinished"></translation>
+        <translation>Ścieżka %1</translation>
     </message>
     <message>
         <source>%1 track(s) successfully uploaded</source>
-        <translation type="unfinished"></translation>
+        <translation>%1 ścieżki wysłane z powodzeniem</translation>
     </message>
     <message>
         <source>upload finished</source>
-        <translation type="unfinished"></translation>
+        <translation>wysyłanie zakończone</translation>
     </message>
     <message>
         <source>current track: %1 - %2</source>
-        <translation type="unfinished"></translation>
+        <translation>bieżący utwór: %1 - %2</translation>
     </message>
     <message>
         <source>no tracks selected</source>
-        <translation type="unfinished"></translation>
+        <translation>nie wybrano żadnej ścieżki</translation>
     </message>
     <message>
         <source>please wait while uploading %1 track(s)</source>
-        <translation type="unfinished"></translation>
+        <translation>proszę czekać trwa wysyłanie %1 ścieżek</translation>
     </message>
     <message>
         <source>upload aborted by the user</source>
-        <translation type="unfinished"></translation>
+        <translation>wysyłanie przerwane przez użytkownika</translation>
     </message>
 </context>
 </TS>
