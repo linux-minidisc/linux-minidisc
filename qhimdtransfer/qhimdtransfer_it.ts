c4f936046d0ddd0a6a79316cf4a3ed97a9281347
Marco Starace <mmwcstarace@googlemail.com>
2009-12-04 17:55:09 +0100

    Added Italian translation.

---
+++ qhimdtransfer/qhimdtransfer_it.ts
@@ -0,0 +1,306 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS><TS version="1.1" language="it">
+<defaultcodec></defaultcodec>
+<context>
+    <name>QHiMDAboutDialog</name>
+    <message>
+        <source>About QHiMDTransfer</source>
+        <translation type="unfinished">Su QHiMD Transfer</translation>
+    </message>
+    <message>
+        <source>&lt;h1&gt;About QHiMDTransfer&lt;/h1&gt;</source>
+        <translation type="unfinished">&lt;h1&gt;Su QHiMD Transfer&lt;/h1&gt;</translation>
+    </message>
+    <message>
+        <source>&lt;h3&gt;QHiMDTransfer, Version 0.0.1&lt;/h3&gt;
+        &lt;p&gt;&lt;p&gt;
+        &lt;p&gt;A free music transfer software for MiniDisc walkman.&lt;/p&gt;
+        &lt;p&gt;&lt;p&gt;
+        &lt;p&gt;Brought to you by the linux-minidisc project:&lt;/p&gt;
+        &lt;p&gt;&lt;p&gt;
+        &lt;p&gt;https://wiki.physik.fu-berlin.de/linux-minidisc&lt;/p&gt;
+        &lt;p&gt;&lt;p&gt;
+        &lt;p&gt;This software is covered by the GNU GPL v2 license. See the
+         file LICENSE in the program folder for more information.&lt;/p&gt;</source>
+        <translation type="unfinished">&lt;h3&gt;QHiMDTransfer, Versione 0.0.1&lt;/h3&gt;
+&lt;p&gt;&lt;p&gt; 
+&lt;p&gt;Un software gratuito da trasferta di musica per MiniDisc walkman.&lt;/p&gt;
+&lt;p&gt;&lt;p&gt;
+&lt;p&gt;Creatp dal linux-minidisc project:&lt;/p&gt;
+&lt;p&gt;&lt;p&gt; 
+&lt;p&gt;https://wiki.physik.fu-berlin.de/linux-minidisc&lt;/p&gt;
+&lt;p&gt;&lt;p&gt; 
+&lt;p&gt;Questo software e coperto dalla licenza GNU GPL v2.
+Vedi il file LICENSE nella directory del programma  per ulteriori informazioni.&lt;/p&gt;</translation>
+    </message>
+    <message>
+        <source>&amp;Close</source>
+        <translation type="unfinished">&amp;Chiudi</translation>
+    </message>
+</context>
+<context>
+    <name>QHiMDFormatDialog</name>
+    <message>
+        <source>Format Medium</source>
+        <translation type="unfinished">Formatta mezzo</translation>
+    </message>
+    <message>
+        <source>Really format MiniDisc (all audio-tracks and data will be lost) ?</source>
+        <translation type="unfinished">Formattare MiniDisc davvero (tutte le tracce audio e tutti i dati saranno persi)?</translation>
+    </message>
+</context>
+<context>
+    <name>QHiMDMainWindow</name>
+    <message>
+        <source>Error opening file for MP3 output</source>
+        <translation type="unfinished">Errore aprendo file per uscita MP3</translation>
+    </message>
+    <message>
+        <source>Error opening track: </source>
+        <translation type="unfinished">Errore aprendo traccia:</translation>
+    </message>
+    <message>
+        <source>Error writing audio data</source>
+        <translation type="unfinished">Errore scrivendo dati audio</translation>
+    </message>
+    <message>
+        <source>upload aborted by the user</source>
+        <translation type="unfinished">upload anullato da utente</translation>
+    </message>
+    <message>
+        <source>Error reading audio data: </source>
+        <translation type="unfinished">Errore leggendo dati audio:</translation>
+    </message>
+    <message>
+        <source>Error opening file for ATRAC output</source>
+        <translation type="unfinished">Errore aprendo file per uscita ATRAC</translation>
+    </message>
+    <message>
+        <source>Error writing header</source>
+        <translation type="unfinished">Errore scrivendo la testata</translation>
+    </message>
+    <message>
+        <source>Error opening file for WAV output</source>
+        <translation type="unfinished">Errore aprendo file per uscita WAV</translation>
+    </message>
+    <message>
+        <source>Error opening HiMD data. Make sure you chose the proper root directory of your HiMD-Walkman.
+</source>
+        <translation type="unfinished">Errore aprendo dati HiMD. Assicurati di aver scelto la giusta root directory del tuo HiMD-Walkman.
+</translation>
+    </message>
+    <message>
+        <source>Track %1</source>
+        <translation type="unfinished">Traccia%1</translation>
+    </message>
+    <message>
+        <source>upload disabled because of DRM encryption</source>
+        <translation type="unfinished">Upload disattivato a causa di crittazione DRM</translation>
+    </message>
+    <message>
+        <source>Select MP3s for download</source>
+        <translation type="unfinished">Seleziona MP3 per download</translation>
+    </message>
+    <message>
+        <source>Select directory for Upload</source>
+        <translation type="unfinished">Seleziona directory per Upload</translation>
+    </message>
+    <message>
+        <source>Select directory of HiMD Medium</source>
+        <translation type="unfinished">Seleziona directory per mezzo HiMD</translation>
+    </message>
+</context>
+<context>
+    <name>QHiMDMainWindowClass</name>
+    <message>
+        <source>QHiMDTransfer</source>
+        <translation type="unfinished">Trasferta QHiMD</translation>
+    </message>
+    <message>
+        <source>HiMD path</source>
+        <translation type="unfinished">Path HiMD</translation>
+    </message>
+    <message>
+        <source>(disconnected)</source>
+        <translation type="unfinished">(disconnesso)</translation>
+    </message>
+    <message>
+        <source>&gt;&gt;</source>
+        <translation type="unfinished">&gt;&gt;</translation>
+    </message>
+    <message>
+        <source>&lt;&lt;</source>
+        <translation type="unfinished">&lt;&lt;</translation>
+    </message>
+    <message>
+        <source>local directory</source>
+        <translation type="unfinished">directory locale</translation>
+    </message>
+    <message>
+        <source>/tmp</source>
+        <translation type="unfinished">/tmp</translation>
+    </message>
+    <message>
+        <source>&amp;File</source>
+        <translation type="unfinished">&amp;File</translation>
+    </message>
+    <message>
+        <source>&amp;?</source>
+        <translation type="unfinished">&amp;?</translation>
+    </message>
+    <message>
+        <source>&amp;Download tracks onto MD</source>
+        <translation type="unfinished">&amp;Download delle traccie sul MD</translation>
+    </message>
+    <message>
+        <source>&amp;Upload tracks from MD</source>
+        <translation type="unfinished">&amp;Upload tracce dal MD</translation>
+    </message>
+    <message>
+        <source>Re&amp;name</source>
+        <translation type="unfinished">Ri&amp;batezza</translation>
+    </message>
+    <message>
+        <source>Rename selected</source>
+        <translation type="unfinished">Ribatezza selezionati</translation>
+    </message>
+    <message>
+        <source>D&amp;elete selected</source>
+        <translation type="unfinished">C&amp;ancella selezzionato</translation>
+    </message>
+    <message>
+        <source>&amp;Help</source>
+        <translation type="unfinished">&amp;Aiuto</translation>
+    </message>
+    <message>
+        <source>&amp;About</source>
+        <translation type="unfinished">&amp;Su</translation>
+    </message>
+    <message>
+        <source>&amp;Format MD</source>
+        <translation type="unfinished">&amp;Formatta MD</translation>
+    </message>
+    <message>
+        <source>Format MD</source>
+        <translation type="unfinished">Formatta MD</translation>
+    </message>
+    <message>
+        <source>&amp;Quit</source>
+        <translation type="unfinished">&amp;Esci</translation>
+    </message>
+    <message>
+        <source>&amp;Connect</source>
+        <translation type="unfinished">&amp;Connetti</translation>
+    </message>
+</context>
+<context>
+    <name>QHiMDTracksModel</name>
+    <message>
+        <source>Nr.</source>
+        <translation type="unfinished">N°</translation>
+    </message>
+    <message>
+        <source>Title</source>
+        <translation type="unfinished">Titolo</translation>
+    </message>
+    <message>
+        <source>Artist</source>
+        <translation type="unfinished">Artista</translation>
+    </message>
+    <message>
+        <source>Album</source>
+        <translation type="unfinished">Albo</translation>
+    </message>
+    <message>
+        <source>Length</source>
+        <translation type="unfinished">Lungezza</translation>
+    </message>
+    <message>
+        <source>Format</source>
+        <translation type="unfinished">Formato</translation>
+    </message>
+    <message>
+        <source>Uploadable</source>
+        <translation type="unfinished">Upload possibile</translation>
+    </message>
+</context>
+<context>
+    <name>QHiMDUploadDialog</name>
+    <message>
+        <source>Track upload - QHiMDTransfer</source>
+        <translation type="unfinished">Upload di traccia - Trasferta QHMiD</translation>
+    </message>
+    <message>
+        <source>please wait while uploading XX track(s)</source>
+        <translation type="unfinished">Attendi prego mentre upload di XX tracccie e in corso</translation>
+    </message>
+    <message>
+        <source>overall progress</source>
+        <translation type="unfinished">Progresso totale</translation>
+    </message>
+    <message>
+        <source>current track: trknum - filename</source>
+        <translation type="unfinished">traccia attuale: trknum - filename</translation>
+    </message>
+    <message>
+        <source>xx track(s) successfully uploaded</source>
+        <translation type="unfinished">upload di xx traccie riuscito</translation>
+    </message>
+    <message>
+        <source>xx track(s) could not be uploaded</source>
+        <translation type="unfinished">upload di xx tracce non potreva essere effetuato</translation>
+    </message>
+    <message>
+        <source>Show Details</source>
+        <translation type="unfinished">Mostra dettagli</translation>
+    </message>
+    <message>
+        <source>Cancel</source>
+        <translation type="unfinished">Annulla</translation>
+    </message>
+    <message>
+        <source>Close</source>
+        <translation type="unfinished">Chiudi</translation>
+    </message>
+    <message>
+        <source>Tracknr.</source>
+        <translation type="unfinished">N° di traccia</translation>
+    </message>
+    <message>
+        <source>Error message</source>
+        <translation type="unfinished">Messaggio di errore</translation>
+    </message>
+    <message>
+        <source>%1 track(s) could not be uploaded</source>
+        <translation type="unfinished">Upload di %1 traccie non poteva essere effetuato</translation>
+    </message>
+    <message>
+        <source>Track %1</source>
+        <translation type="unfinished">Traccia%1</translation>
+    </message>
+    <message>
+        <source>%1 track(s) successfully uploaded</source>
+        <translation type="unfinished">Upload di %1 traccie riuscito</translation>
+    </message>
+    <message>
+        <source>upload finished</source>
+        <translation type="unfinished">upload finito</translation>
+    </message>
+    <message>
+        <source>current track: %1 - %2</source>
+        <translation type="unfinished">traccia attuale: %1 - %2</translation>
+    </message>
+    <message>
+        <source>no tracks selected</source>
+        <translation type="unfinished">Nessuna traccia selezionata</translation>
+    </message>
+    <message>
+        <source>please wait while uploading %1 track(s)</source>
+        <translation type="unfinished">Attendi prego mentre upload di %1 traccie e completo</translation>
+    </message>
+    <message>
+        <source>upload aborted by the user</source>
+        <translation type="unfinished">upload annullato da utente</translation>
+    </message>
+</context>
+</TS>
