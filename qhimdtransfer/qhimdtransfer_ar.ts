76fe0f89dc5afca4507143f31578b711184a90cd
Hossam Elgabarty <elgabart@zedat.fu-berlin.de>
2010-01-10 12:18:39 +0100

    Added Arabian translation.

---
+++ qhimdtransfer/qhimdtransfer_ar.ts
@@ -0,0 +1,306 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="ar" sourcelanguage="en">
+<context>
+    <name>QHiMDAboutDialog</name>
+    <message>
+        <source>About QHiMDTransfer</source>
+        <translation>عن QHIMDTransfer</translation>
+    </message>
+    <message>
+        <source>&lt;h1&gt;About QHiMDTransfer&lt;/h1&gt;</source>
+        <translation>&lt;h1&gt;عن QHIMDTransfer&lt;h1&gt;</translation>
+    </message>
+    <message>
+        <source>&lt;h3&gt;QHiMDTransfer, Version 0.0.1&lt;/h3&gt;
+        &lt;p&gt;&lt;p&gt;
+        &lt;p&gt;A free music transfer software for MiniDisc walkman.&lt;/p&gt;
+        &lt;p&gt;&lt;p&gt;
+        &lt;p&gt;Brought to you by the linux-minidisc project:&lt;/p&gt;
+        &lt;p&gt;&lt;p&gt;
+        &lt;p&gt;https://wiki.physik.fu-berlin.de/linux-minidisc&lt;/p&gt;
+        &lt;p&gt;&lt;p&gt;
+        &lt;p&gt;This software is covered by the GNU GPL v2 license. See the
+         file LICENSE in the program folder for more information.&lt;/p&gt;</source>
+        <translation>&lt;h3&gt;QHiMDTransfer, الإصدار 0.0.1&lt;/h3&gt;
+        &lt;p&gt;&lt;p&gt;
+        &lt;p&gt;برنامج مجانى لنقل الموسيقى للأجهزة النقالة&lt;/p&gt;
+        &lt;p&gt;&lt;p&gt;
+        &lt;p&gt;أصدره إليكم مشروع linux-minidisc:&lt;/p&gt;
+        &lt;p&gt;&lt;p&gt;
+        &lt;p&gt;https://wiki.physik.fu-berlin.de/linux-minidisc&lt;/p&gt;
+        &lt;p&gt;&lt;p&gt;
+        &lt;p&gt;هذا البرنامج مغطى برخصة GNU GPL v2. أنظر الملف
+         LICENSE للمزيد من المعلومات.&lt;/p&gt;</translation>
+    </message>
+    <message>
+        <source>&amp;Close</source>
+        <translation></translation>
+    </message>
+</context>
+<context>
+    <name>QHiMDFormatDialog</name>
+    <message>
+        <source>Format Medium</source>
+        <translation>تهيئة الوسط</translation>
+    </message>
+    <message>
+        <source>Really format MiniDisc (all audio-tracks and data will be lost) ?</source>
+        <translation>هل أنت واثق (ستفقد كل البيانات) ؟</translation>
+    </message>
+</context>
+<context>
+    <name>QHiMDMainWindow</name>
+    <message>
+        <source>Error opening file for MP3 output</source>
+        <translation>خطأ فى فتح الملف لكتابة بيانات الصوت</translation>
+    </message>
+    <message>
+        <source>Error opening track: </source>
+        <translation>‫خطأ فى فتح الملف الصوتى:</translation>
+    </message>
+    <message>
+        <source>Error writing audio data</source>
+        <translation>‫خطأ فى كتابة بيانات الصوت</translation>
+    </message>
+    <message>
+        <source>upload aborted by the user</source>
+        <translation>تم إلغاء الرفع بواسطة المستخدم</translation>
+    </message>
+    <message>
+        <source>Error reading audio data: </source>
+        <translation>‫خطأ فى قراءة بيانات الصوت:</translation>
+    </message>
+    <message>
+        <source>Error opening file for ATRAC output</source>
+        <translation>ATRAC خطأ فى فتح الملف لكتابة </translation>
+    </message>
+    <message>
+        <source>Error writing header</source>
+        <translation>‫خطأ فى قراءة رأس البيانات</translation>
+    </message>
+    <message>
+        <source>Error opening file for WAV output</source>
+        <translation>WAV خطأ فى فتح الملف لكتابة </translation>
+    </message>
+    <message>
+        <source>Error opening HiMD data. Make sure you chose the proper root directory of your HiMD-Walkman.
+</source>
+        <translation>خطأ فى كتابة البيانات
+ ‮‫تأكد من اختيار الحافظة الرئيسية فى الجهاز النقال الخاص بك.</translation>
+    </message>
+    <message>
+        <source>Track %1</source>
+        <translation>الملف %1</translation>
+    </message>
+    <message>
+        <source>upload disabled because of DRM encryption</source>
+        <translation>تم إيقاف قابلية التحميل بسبب تشفير ‏‪DRM</translation>
+    </message>
+    <message>
+        <source>Select MP3s for download</source>
+        <translation>اختر الملفات الصوتية للتحميل</translation>
+    </message>
+    <message>
+        <source>Select directory for Upload</source>
+        <translation>اختر الحافظة للتحميل</translation>
+    </message>
+    <message>
+        <source>Select directory of HiMD Medium</source>
+        <translation>اختر الحافظة لوسط HiMD</translation>
+    </message>
+</context>
+<context>
+    <name>QHiMDMainWindowClass</name>
+    <message>
+        <source>QHiMDTransfer</source>
+        <translation>QHiMDTransfer</translation>
+    </message>
+    <message>
+        <source>HiMD path</source>
+        <translation>HiMD path</translation>
+    </message>
+    <message>
+        <source>(disconnected)</source>
+        <translation>(غير متصل)</translation>
+    </message>
+    <message>
+        <source>&gt;&gt;</source>
+        <translation>&gt;&gt;</translation>
+    </message>
+    <message>
+        <source>&lt;&lt;</source>
+        <translation>&lt;&lt;</translation>
+    </message>
+    <message>
+        <source>local directory</source>
+        <translation>حافظة محلية</translation>
+    </message>
+    <message>
+        <source>/tmp</source>
+        <translation>/tmp</translation>
+    </message>
+    <message>
+        <source>&amp;File</source>
+        <translation>ملف(&amp;F)</translation>
+    </message>
+    <message>
+        <source>&amp;?</source>
+        <translation>&amp;?</translation>
+    </message>
+    <message>
+        <source>&amp;Download tracks onto MD</source>
+        <translation>تحميل الملفات على ‪MD(&amp;D)</translation>
+    </message>
+    <message>
+        <source>&amp;Upload tracks from MD</source>
+        <translation>رفع الملفات على ‪MD(&amp;U)</translation>
+    </message>
+    <message>
+        <source>Re&amp;name</source>
+        <translation>إعادة تسمية ‪(&amp;n)</translation>
+    </message>
+    <message>
+        <source>Rename selected</source>
+        <translation>إعادة تسمية الاختيار‪</translation>
+    </message>
+    <message>
+        <source>D&amp;elete selected</source>
+        <translation>مسح الاختيار‪ ‪(&amp;D)</translation>
+    </message>
+    <message>
+        <source>&amp;Help</source>
+        <translation>مساعدة ‪(&amp;H)</translation>
+    </message>
+    <message>
+        <source>&amp;About</source>
+        <translation>عن البرنامج ‪(&amp;A)</translation>
+    </message>
+    <message>
+        <source>&amp;Format MD</source>
+        <translation>تهيئة ‪MD (&amp;F)</translation>
+    </message>
+    <message>
+        <source>Format MD</source>
+        <translation>تهيئة ‪MD</translation>
+    </message>
+    <message>
+        <source>&amp;Quit</source>
+        <translation>خروج ‪(&amp;Q)</translation>
+    </message>
+    <message>
+        <source>&amp;Connect</source>
+        <translation>اتصال ‪(&amp;C)</translation>
+    </message>
+</context>
+<context>
+    <name>QHiMDTracksModel</name>
+    <message>
+        <source>Nr.</source>
+        <translation>رقم</translation>
+    </message>
+    <message>
+        <source>Title</source>
+        <translation>الاسم</translation>
+    </message>
+    <message>
+        <source>Artist</source>
+        <translation>الفنان</translation>
+    </message>
+    <message>
+        <source>Album</source>
+        <translation>الألبوم</translation>
+    </message>
+    <message>
+        <source>Length</source>
+        <translation>المدة</translation>
+    </message>
+    <message>
+        <source>Format</source>
+        <translation>تهيئة</translation>
+    </message>
+    <message>
+        <source>Uploadable</source>
+        <translation>قابل للرفع</translation>
+    </message>
+</context>
+<context>
+    <name>QHiMDUploadDialog</name>
+    <message>
+        <source>Track upload - QHiMDTransfer</source>
+        <translation>رفع ملف : ‪QHiMD Transfer</translation>
+    </message>
+    <message>
+        <source>please wait while uploading XX track(s)</source>
+        <translation>رجاء الانتظار حتى يتم تحميل xx ملف</translation>
+    </message>
+    <message>
+        <source>overall progress</source>
+        <translation>تم</translation>
+    </message>
+    <message>
+        <source>current track: trknum - filename</source>
+        <translation>الملف الحالى: رقم - اسم الملف</translation>
+    </message>
+    <message>
+        <source>xx track(s) successfully uploaded</source>
+        <translation>‫xx ملف تم تحميلها</translation>
+    </message>
+    <message>
+        <source>xx track(s) could not be uploaded</source>
+        <translation>‫xx ملف لم يتم تحميلها</translation>
+    </message>
+    <message>
+        <source>Show Details</source>
+        <translation>اظهر التفاصيل</translation>
+    </message>
+    <message>
+        <source>Cancel</source>
+        <translation>إلغاء</translation>
+    </message>
+    <message>
+        <source>Close</source>
+        <translation>غلق</translation>
+    </message>
+    <message>
+        <source>Tracknr.</source>
+        <translation>رقم الملف</translation>
+    </message>
+    <message>
+        <source>Error message</source>
+        <translation>رسالة بحدوث خطأ</translation>
+    </message>
+    <message>
+        <source>%1 track(s) could not be uploaded</source>
+        <translation>%1 ملف لم يمكن تحميلها</translation>
+    </message>
+    <message>
+        <source>Track %1</source>
+        <translation>ملف %1</translation>
+    </message>
+    <message>
+        <source>%1 track(s) successfully uploaded</source>
+        <translation>%1 ملف نم تحميلها</translation>
+    </message>
+    <message>
+        <source>upload finished</source>
+        <translation>تم الرفع</translation>
+    </message>
+    <message>
+        <source>current track: %1 - %2</source>
+        <translation>الملف الحالى: %1-%2</translation>
+    </message>
+    <message>
+        <source>no tracks selected</source>
+        <translation>لم يتم إختيار أى ملف</translation>
+    </message>
+    <message>
+        <source>please wait while uploading %1 track(s)</source>
+        <translation>رجاء الانتظار حتى يتم تحميل %1 ملف</translation>
+    </message>
+    <message>
+        <source>upload aborted by the user</source>
+        <translation>تم إلغاء الرفع بواسطة المستخدم</translation>
+    </message>
+</context>
+</TS>
