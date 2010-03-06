bb8f44ab7e5a6dd19bdae68765e08905ecc68c6a
Peter Kyster Slot <pkslot@gmail.com>
2010-03-06 21:16:18 +0100

    Added Danish translation for qhimdtransfer.

---
+++ qhimdtransfer/qhimdtransfer_da.ts
@@ -0,0 +1,294 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="da_DK">
+<context>
+    <name>QHiMDAboutDialog</name>
+    <message>
+        <source>About QHiMDTransfer</source>
+        <translation>Om QHiMDTransfer</translation>
+    </message>
+    <message>
+        <source>&lt;h1&gt;About QHiMDTransfer&lt;/h1&gt;</source>
+        <translation>&lt;h1&gt;Om QHiMDTransfer &lt;/h1&gt;</translation>
+    </message>
+    <message>
+        <source>&lt;h3&gt;QHiMDTransfer, Version 0.0.1&lt;/h3&gt;
+        &lt;p&gt;&lt;p&gt;
+        &lt;p&gt;A free music transfer software for MiniDisc walkman.&lt;/p&gt;
+        &lt;p&gt;&lt;p&gt;
+        &lt;p&gt;Brought to you by the linux-minidisc project:&lt;/p&gt;
+        &lt;p&gt;&lt;p&gt;
+        &lt;p&gt;https://wiki.physik.fu-berlin.de/linux-minidisc&lt;/p&gt;
+        &lt;p&gt;&lt;p&gt;
+        &lt;p&gt;This software is covered by the GNU GPL v2 license. See the
+         file LICENSE in the program folder for more information.&lt;/p&gt;</source>
+        <translation>&lt;h3&gt;QHiMDTransfer, Version 0.0.1&lt;/h3&gt;
+        &lt;p&gt;&lt;p&gt;
+        &lt;p&gt;En gratis musik overføringssoftware til MiniDisc walkman.&lt;/p&gt;
+        &lt;p&gt;&lt;p&gt;
+        &lt;p&gt;Lavet af linux-minidisc projektet:&lt;/p&gt;
+        &lt;p&gt;&lt;p&gt;
+        &lt;p&gt;https://wiki.physik.fu-berlin.de/linux-minidisc&lt;/p&gt;
+        &lt;p&gt;&lt;p&gt;
+        &lt;p&gt;Denne software hører under GNU GPL v2 licensen. Læs
+         file LICENSE i programfolderen for mere info.&lt;/p&gt;</translation>
+    </message>
+    <message>
+        <source>&amp;Close</source>
+        <translation>&amp;Ḷuk</translation>
+    </message>
+</context>
+<context>
+    <name>QHiMDFormatDialog</name>
+    <message>
+        <source>Format Medium</source>
+        <translation>Formatér medie</translation>
+    </message>
+    <message>
+        <source>Really format MiniDisc (all audio-tracks and data will be lost) ?</source>
+        <translation>Er du sikker på, at du vil formatere (alle lydspor og data vil blive slettet) ?</translation>
+    </message>
+</context>
+<context>
+    <name>QHiMDMainWindow</name>
+    <message>
+        <source>Error opening file for MP3 output</source>
+        <translation>Fejl: Kan ikke åbne fil for MP3 uddata</translation>
+    </message>
+    <message>
+        <source>Error opening track: </source>
+        <translation>Fejl: kunne ikke åbne filen:</translation>
+    </message>
+    <message>
+        <source>Error writing audio data</source>
+        <translation>Fejl: kunne ikke skrive audio data</translation>
+    </message>
+    <message>
+        <source>upload aborted by the user</source>
+        <translation>Upload avbrudt af bruger</translation>
+    </message>
+    <message>
+        <source>Error reading audio data: </source>
+        <translation>Fejl: kunne ikke læse audio data:</translation>
+    </message>
+    <message>
+        <source>Error opening file for ATRAC output</source>
+        <translation>Fejl: Kan ikke åbne fil for ATRAC uddata</translation>
+    </message>
+    <message>
+        <source>Error writing header</source>
+        <translation>Fejl, kunne ikke skrive header</translation>
+    </message>
+    <message>
+        <source>Error opening file for WAV output</source>
+        <translation>Fejl: Kan ikke åbne fil for WAV uddata</translation>
+    </message>
+    <message>
+        <source>Select MP3s for download</source>
+        <translation>Vælg MP3 filen du vil downloade</translation>
+    </message>
+    <message>
+        <source>Select directory for Upload</source>
+        <translation>Vælg en mappe du vil uploade</translation>
+    </message>
+    <message>
+        <source>Track %1</source>
+        <translation>Spor %1</translation>
+    </message>
+    <message>
+        <source>upload disabled because of DRM encryption</source>
+        <translation>Kunne ikke uploades på grund af DRM kryptering</translation>
+    </message>
+    <message>
+        <source>Select directory of HiMD Medium</source>
+        <translation>Vælg destination for HiMD Medie</translation>
+    </message>
+    <message>
+        <source>Error opening HiMD data. Make sure you chose the proper root directory of your HiMD-Walkman.
+</source>
+        <translation>Fejl: kunne ikke åbne HIMD data. Kontrollér at du har valgt den rette destination for din HIMD Walkman.
+</translation>
+    </message>
+</context>
+<context>
+    <name>QHiMDMainWindowClass</name>
+    <message>
+        <source>QHiMDTransfer</source>
+        <translation>QHiMDTransfer</translation>
+    </message>
+    <message>
+        <source>&amp;File</source>
+        <translation>&amp;Fil</translation>
+    </message>
+    <message>
+        <source>&amp;?</source>
+        <translation>&amp;?</translation>
+    </message>
+    <message>
+        <source>&amp;Download tracks onto MD</source>
+        <translation>&amp;Ḥent lydspor til din MD</translation>
+    </message>
+    <message>
+        <source>&amp;Upload tracks from MD</source>
+        <translation>&amp;Ṣend lydspor fra din MD</translation>
+    </message>
+    <message>
+        <source>Re&amp;name</source>
+        <translation>&amp;Ọmdøb</translation>
+    </message>
+    <message>
+        <source>Rename selected</source>
+        <translation>Omdøb valgte</translation>
+    </message>
+    <message>
+        <source>D&amp;elete selected</source>
+        <translation>&amp;Ṣlet valgte</translation>
+    </message>
+    <message>
+        <source>&amp;Help</source>
+        <translation>&amp;Hjælp</translation>
+    </message>
+    <message>
+        <source>&amp;About</source>
+        <translation>&amp;Om</translation>
+    </message>
+    <message>
+        <source>&amp;Format MD</source>
+        <translation>&amp;Formatér MD</translation>
+    </message>
+    <message>
+        <source>Format MD</source>
+        <translation>Formatér MD</translation>
+    </message>
+    <message>
+        <source>&amp;Quit</source>
+        <translation>&amp;Ḷuk</translation>
+    </message>
+    <message>
+        <source>&amp;Connect</source>
+        <translation>&amp;Forbind</translation>
+    </message>
+    <message>
+        <source>HiMD path</source>
+        <translation>HiMD destination</translation>
+    </message>
+    <message>
+        <source>(disconnected)</source>
+        <translation>(ikke forbundet)</translation>
+    </message>
+    <message>
+        <source>local directory</source>
+        <translation>Lokal mappe</translation>
+    </message>
+</context>
+<context>
+    <name>QHiMDTracksModel</name>
+    <message>
+        <source>Nr.</source>
+        <translation>Nr.</translation>
+    </message>
+    <message>
+        <source>Title</source>
+        <translation>Titel</translation>
+    </message>
+    <message>
+        <source>Artist</source>
+        <translation>Artist</translation>
+    </message>
+    <message>
+        <source>Album</source>
+        <translation>Album</translation>
+    </message>
+    <message>
+        <source>Length</source>
+        <translation>Længde</translation>
+    </message>
+    <message>
+        <source>Format</source>
+        <translation>Format</translation>
+    </message>
+    <message>
+        <source>Uploadable</source>
+        <translation>Kan uploades</translation>
+    </message>
+</context>
+<context>
+    <name>QHiMDUploadDialog</name>
+    <message>
+        <source>%1 track(s) could not be uploaded</source>
+        <translation>%1 spor kunne ikke uploades</translation>
+    </message>
+    <message>
+        <source>Track %1</source>
+        <translation>Spor %1</translation>
+    </message>
+    <message>
+        <source>%1 track(s) successfully uploaded</source>
+        <translation>%1 spor blev uploaded</translation>
+    </message>
+    <message>
+        <source>upload finished</source>
+        <translation>upload afsluttet</translation>
+    </message>
+    <message>
+        <source>current track: %1 - %2</source>
+        <translation>nuværende spor: %1 - %2</translation>
+    </message>
+    <message>
+        <source>no tracks selected</source>
+        <translation>Ingen valgte spor</translation>
+    </message>
+    <message>
+        <source>please wait while uploading %1 track(s)</source>
+        <translation>Vent venligst mens %1 spor bliver uploaded</translation>
+    </message>
+    <message>
+        <source>upload aborted by the user</source>
+        <translation>Upload afbrudt af bruger</translation>
+    </message>
+    <message>
+        <source>Track upload - QHiMDTransfer</source>
+        <translation>Upload - QHiMDTransfer</translation>
+    </message>
+    <message>
+        <source>please wait while uploading XX track(s)</source>
+        <translation>uploader XX spor, vent venligst</translation>
+    </message>
+    <message>
+        <source>overall progress</source>
+        <translation>fremgang</translation>
+    </message>
+    <message>
+        <source>current track: trknum - filename</source>
+        <translation>nuværende spor: trknum - filnavn</translation>
+    </message>
+    <message>
+        <source>xx track(s) successfully uploaded</source>
+        <translation>xx spor blev uploadet</translation>
+    </message>
+    <message>
+        <source>xx track(s) could not be uploaded</source>
+        <translation>xx spor kunne ikke uploades</translation>
+    </message>
+    <message>
+        <source>Show Details</source>
+        <translation>Vis Detaljer</translation>
+    </message>
+    <message>
+        <source>Cancel</source>
+        <translation>Annulér</translation>
+    </message>
+    <message>
+        <source>Close</source>
+        <translation>Luk</translation>
+    </message>
+    <message>
+        <source>Tracknr.</source>
+        <translation>Spornr.</translation>
+    </message>
+    <message>
+        <source>Error message</source>
+        <translation>Fejl besked</translation>
+    </message>
+</context>
+</TS>
