b9ab4969a5c41e6b54dddedb0f45b0af2a67ed71
Pliubis Apostolos <liquidxd@gmail.com>
2010-05-26 21:38:05 +0200

    Updated Greek translation.

--- qhimdtransfer/qhimdtransfer_el.ts
+++ qhimdtransfer/qhimdtransfer_el.ts
@@ -9,7 +9,7 @@
     </message>
     <message>
         <source>&lt;h1&gt;About QHiMDTransfer&lt;/h1&gt;</source>
-        <translation>&lt;h1&gt;Σχετικά με QHiMDTransfer&lt;/h1&gt;</translation>
+        <translation type="obsolete">&lt;h1&gt;Σχετικά με QHiMDTransfer&lt;/h1&gt;</translation>
     </message>
     <message>
         <source>&lt;h3&gt;QHiMDTransfer, Version 0.0.1&lt;/h3&gt;
@@ -36,6 +36,14 @@
     <message>
         <source>&amp;Close</source>
         <translation>&amp;Κλείσιμο</translation>
+    </message>
+    <message>
+        <source>About</source>
+        <translation>Σχετικά</translation>
+    </message>
+    <message>
+        <source>Contributors</source>
+        <translation>Συνεισφέροντες</translation>
     </message>
 </context>
 <context>
@@ -108,6 +116,10 @@
         <source>Select directory of HiMD Medium</source>
         <translation>Επιλέξτε τον κατάλογο που βρίσκεται το HiMD</translation>
     </message>
+    <message>
+        <source>(disconnected)</source>
+        <translation>(αποσυνδεδεμένος)</translation>
+    </message>
 </context>
 <context>
     <name>QHiMDMainWindowClass</name>
@@ -133,7 +145,7 @@
     </message>
     <message>
         <source>local directory</source>
-        <translation>τοπικός κατάλογος</translation>
+        <translation>Tοπικός κατάλογος</translation>
     </message>
     <message>
         <source>/tmp</source>
