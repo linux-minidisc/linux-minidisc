591d1d8ebda8cfe1129187ec5e966b6cdbde756c
Máximo Antonio Chicano Díaz <taliesinbardwolf@gmail.com>
2010-11-09 14:52:04 +0100

    Add Spanish translation for qhimdtransfer - forgot translation file.

---
+++ qhimdtransfer/qhimdtransfer_es.ts
@@ -0,0 +1,361 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="es_ES">
+<context>
+    <name>QHiMDAboutDialog</name>
+    <message>
+        <location filename="qhimdaboutdialog.ui" line="35"/>
+        <source>About QHiMDTransfer</source>
+        <translation>Acerca de QHIMDTransfer</translation>
+    </message>
+    <message>
+        <location filename="qhimdaboutdialog.ui" line="70"/>
+        <source>About</source>
+        <translation>Acerca de</translation>
+    </message>
+    <message>
+        <location filename="qhimdaboutdialog.ui" line="82"/>
+        <source>&lt;h3&gt;QHiMDTransfer, Version 0.0.1&lt;/h3&gt;
+        &lt;p&gt;&lt;p&gt;
+        &lt;p&gt;A free music transfer software for MiniDisc walkman.&lt;/p&gt;
+        &lt;p&gt;&lt;p&gt;
+        &lt;p&gt;Brought to you by the linux-minidisc project:&lt;/p&gt;
+        &lt;p&gt;&lt;p&gt;
+        &lt;p&gt;https://wiki.physik.fu-berlin.de/linux-minidisc&lt;/p&gt;
+        &lt;p&gt;&lt;p&gt;
+        &lt;p&gt;This software is covered by the GNU GPL v2 license. See the
+         file LICENSE in the program folder for more information.&lt;/p&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qhimdaboutdialog.ui" line="97"/>
+        <source>Contributors</source>
+        <translation>Han contribuído</translation>
+    </message>
+    <message>
+        <location filename="qhimdaboutdialog.ui" line="152"/>
+        <source>&amp;Close</source>
+        <translation>&amp;Cerrar</translation>
+    </message>
+</context>
+<context>
+    <name>QHiMDFormatDialog</name>
+    <message>
+        <location filename="qhimdformatdialog.ui" line="14"/>
+        <source>Format Medium</source>
+        <translation>Dar formato al disco MD</translation>
+    </message>
+    <message>
+        <location filename="qhimdformatdialog.ui" line="41"/>
+        <source>Really format MiniDisc (all audio-tracks and data will be lost) ?</source>
+        <translation>¿Realmente quiere dar formato al Minidisc (todos los datos y las pistas de audio se perderán)?</translation>
+    </message>
+</context>
+<context>
+    <name>QHiMDMainWindow</name>
+    <message>
+        <location filename="qhimdmainwindow.cpp" line="22"/>
+        <source>Error opening file for MP3 output</source>
+        <translation>Error de apertura de archivo para salida en MP3</translation>
+    </message>
+    <message>
+        <location filename="qhimdmainwindow.cpp" line="27"/>
+        <location filename="qhimdmainwindow.cpp" line="91"/>
+        <location filename="qhimdmainwindow.cpp" line="151"/>
+        <source>Error opening track: </source>
+        <translation>Error de apertura de pista:</translation>
+    </message>
+    <message>
+        <location filename="qhimdmainwindow.cpp" line="33"/>
+        <location filename="qhimdmainwindow.cpp" line="103"/>
+        <location filename="qhimdmainwindow.cpp" line="171"/>
+        <source>Error writing audio data</source>
+        <translation>Error de escritura de datos de audio</translation>
+    </message>
+    <message>
+        <location filename="qhimdmainwindow.cpp" line="40"/>
+        <source>upload aborted by the user</source>
+        <translation>Subida abortada por el usuario</translation>
+    </message>
+    <message>
+        <location filename="qhimdmainwindow.cpp" line="46"/>
+        <source>Error reading audio data: </source>
+        <translation>Error de lectura de datos de audio:</translation>
+    </message>
+    <message>
+        <location filename="qhimdmainwindow.cpp" line="86"/>
+        <source>Error opening file for ATRAC output</source>
+        <translation>Error de apertura de archivo para salida en ATRAC</translation>
+    </message>
+    <message>
+        <location filename="qhimdmainwindow.cpp" line="96"/>
+        <source>Error writing header</source>
+        <translation>Error de escritura de cabecera</translation>
+    </message>
+    <message>
+        <location filename="qhimdmainwindow.cpp" line="146"/>
+        <source>Error opening file for WAV output</source>
+        <translation>Error de apertura de archivo para salida en WAV</translation>
+    </message>
+    <message>
+        <location filename="qhimdmainwindow.cpp" line="303"/>
+        <source>Error opening HiMD data. Make sure you chose the proper root directory of your HiMD-Walkman.
+</source>
+        <translation>Error de apertura de datos del HiMD. Asegúrese de que ha elegido la ruta correcta al directorio raíz.</translation>
+    </message>
+    <message>
+        <location filename="qhimdmainwindow.cpp" line="338"/>
+        <source>Track %1</source>
+        <translation>Pista %1</translation>
+    </message>
+    <message>
+        <location filename="qhimdmainwindow.cpp" line="365"/>
+        <source>upload disabled because of DRM encryption</source>
+        <translation>Subida deshabilitada por encriptación DRM</translation>
+    </message>
+    <message>
+        <location filename="qhimdmainwindow.cpp" line="415"/>
+        <source>Select MP3s for download</source>
+        <translation>Seleccione MP3s para descargar</translation>
+    </message>
+    <message>
+        <location filename="qhimdmainwindow.cpp" line="425"/>
+        <source>Select directory for Upload</source>
+        <translation>Seleccione directorio para subida</translation>
+    </message>
+    <message>
+        <location filename="qhimdmainwindow.cpp" line="457"/>
+        <source>Select directory of HiMD Medium</source>
+        <translation>Seleccione directorio del disco HiMD</translation>
+    </message>
+    <message>
+        <location filename="qhimdmainwindow.cpp" line="536"/>
+        <source>(disconnected)</source>
+        <translation>(desconectado)</translation>
+    </message>
+</context>
+<context>
+    <name>QHiMDMainWindowClass</name>
+    <message>
+        <location filename="qhimdmainwindow.ui" line="14"/>
+        <source>QHiMDTransfer</source>
+        <translation>QHiMDTransfer</translation>
+    </message>
+    <message>
+        <location filename="qhimdmainwindow.ui" line="38"/>
+        <source>HiMD path</source>
+        <translation>Ruta del HiMD</translation>
+    </message>
+    <message>
+        <location filename="qhimdmainwindow.ui" line="45"/>
+        <source>(disconnected)</source>
+        <translation>(desconectado)</translation>
+    </message>
+    <message>
+        <location filename="qhimdmainwindow.ui" line="138"/>
+        <source>local directory</source>
+        <translation>Directorio local</translation>
+    </message>
+    <message>
+        <location filename="qhimdmainwindow.ui" line="176"/>
+        <source>&amp;File</source>
+        <translation>&amp;Archivo</translation>
+    </message>
+    <message>
+        <location filename="qhimdmainwindow.ui" line="192"/>
+        <source>&amp;?</source>
+        <translation>&amp;?</translation>
+    </message>
+    <message>
+        <location filename="qhimdmainwindow.ui" line="229"/>
+        <source>&amp;Download tracks onto MD</source>
+        <translation>&amp;Descargar canciones al MD</translation>
+    </message>
+    <message>
+        <location filename="qhimdmainwindow.ui" line="238"/>
+        <source>&amp;Upload tracks from MD</source>
+        <translation>&amp;Subir canciones desde el el MD</translation>
+    </message>
+    <message>
+        <location filename="qhimdmainwindow.ui" line="247"/>
+        <source>Re&amp;name</source>
+        <translation>&amp;Renombrar</translation>
+    </message>
+    <message>
+        <location filename="qhimdmainwindow.ui" line="250"/>
+        <source>Rename selected</source>
+        <translation>Renombrar selección</translation>
+    </message>
+    <message>
+        <location filename="qhimdmainwindow.ui" line="259"/>
+        <source>D&amp;elete selected</source>
+        <translation>&amp;Borrar selección</translation>
+    </message>
+    <message>
+        <location filename="qhimdmainwindow.ui" line="268"/>
+        <source>&amp;Help</source>
+        <translation>A&amp;yuda</translation>
+    </message>
+    <message>
+        <location filename="qhimdmainwindow.ui" line="277"/>
+        <source>&amp;About</source>
+        <translation>Ac&amp;erca de</translation>
+    </message>
+    <message>
+        <location filename="qhimdmainwindow.ui" line="286"/>
+        <source>&amp;Format MD</source>
+        <translation>Dar &amp;formato al MD</translation>
+    </message>
+    <message>
+        <location filename="qhimdmainwindow.ui" line="289"/>
+        <source>Format MD</source>
+        <translation>Dar formato al MD</translation>
+    </message>
+    <message>
+        <location filename="qhimdmainwindow.ui" line="298"/>
+        <source>&amp;Quit</source>
+        <translation>&amp;Salir</translation>
+    </message>
+    <message>
+        <location filename="qhimdmainwindow.ui" line="307"/>
+        <source>&amp;Connect</source>
+        <translation>&amp;Conectar</translation>
+    </message>
+</context>
+<context>
+    <name>QHiMDTracksModel</name>
+    <message>
+        <location filename="qhimdmodel.cpp" line="149"/>
+        <source>Nr.</source>
+        <translation>Nr.</translation>
+    </message>
+    <message>
+        <location filename="qhimdmodel.cpp" line="151"/>
+        <source>Title</source>
+        <translation>Título</translation>
+    </message>
+    <message>
+        <location filename="qhimdmodel.cpp" line="153"/>
+        <source>Artist</source>
+        <translation>Artista</translation>
+    </message>
+    <message>
+        <location filename="qhimdmodel.cpp" line="155"/>
+        <source>Album</source>
+        <translation>Álbum</translation>
+    </message>
+    <message>
+        <location filename="qhimdmodel.cpp" line="157"/>
+        <source>Length</source>
+        <translation>Duración</translation>
+    </message>
+    <message>
+        <location filename="qhimdmodel.cpp" line="159"/>
+        <source>Format</source>
+        <translation type="unfinished">Formato</translation>
+    </message>
+    <message>
+        <location filename="qhimdmodel.cpp" line="161"/>
+        <source>Uploadable</source>
+        <translation>Apto para subida</translation>
+    </message>
+</context>
+<context>
+    <name>QHiMDUploadDialog</name>
+    <message>
+        <location filename="qhimduploaddialog.ui" line="14"/>
+        <source>Track upload - QHiMDTransfer</source>
+        <translation>Subida de pista - QHiMDTransfer</translation>
+    </message>
+    <message>
+        <location filename="qhimduploaddialog.ui" line="55"/>
+        <source>please wait while uploading XX track(s)</source>
+        <translation>por favor, expedre mientras se sube(n) XX pista(s)</translation>
+    </message>
+    <message>
+        <location filename="qhimduploaddialog.ui" line="69"/>
+        <source>overall progress</source>
+        <translation>Progreso total</translation>
+    </message>
+    <message>
+        <location filename="qhimduploaddialog.ui" line="93"/>
+        <source>current track: trknum - filename</source>
+        <translation>Pista actual:  trknum – filename</translation>
+    </message>
+    <message>
+        <location filename="qhimduploaddialog.ui" line="108"/>
+        <source>xx track(s) successfully uploaded</source>
+        <translation>xx pista(s) subida(s) con éxito</translation>
+    </message>
+    <message>
+        <location filename="qhimduploaddialog.ui" line="115"/>
+        <source>xx track(s) could not be uploaded</source>
+        <translation>xx pista(s) no ha(n) podido subirse</translation>
+    </message>
+    <message>
+        <location filename="qhimduploaddialog.ui" line="129"/>
+        <source>Show Details</source>
+        <translation>Mostrar detalles</translation>
+    </message>
+    <message>
+        <location filename="qhimduploaddialog.ui" line="142"/>
+        <source>Cancel</source>
+        <translation>Cancelar</translation>
+    </message>
+    <message>
+        <location filename="qhimduploaddialog.ui" line="149"/>
+        <source>Close</source>
+        <translation>Cerrar</translation>
+    </message>
+    <message>
+        <location filename="qhimduploaddialog.ui" line="186"/>
+        <source>Tracknr.</source>
+        <translation>Número de pista.</translation>
+    </message>
+    <message>
+        <location filename="qhimduploaddialog.ui" line="191"/>
+        <source>Error message</source>
+        <translation>Mensaje de error</translation>
+    </message>
+    <message>
+        <location filename="qhimduploaddialog.cpp" line="10"/>
+        <source>%1 track(s) could not be uploaded</source>
+        <translation>%1 pista(s) no ha(n) podido subirse</translation>
+    </message>
+    <message>
+        <location filename="qhimduploaddialog.cpp" line="15"/>
+        <source>Track %1</source>
+        <translation>Pista %1</translation>
+    </message>
+    <message>
+        <location filename="qhimduploaddialog.cpp" line="28"/>
+        <source>%1 track(s) successfully uploaded</source>
+        <translation>%1 pista(s) subidas con éxito</translation>
+    </message>
+    <message>
+        <location filename="qhimduploaddialog.cpp" line="33"/>
+        <source>upload finished</source>
+        <translation>Subida terminada</translation>
+    </message>
+    <message>
+        <location filename="qhimduploaddialog.cpp" line="48"/>
+        <source>current track: %1 - %2</source>
+        <translation>Pista actual: %1 - %2</translation>
+    </message>
+    <message>
+        <location filename="qhimduploaddialog.cpp" line="74"/>
+        <source>no tracks selected</source>
+        <translation>No hay pistas seleccionadas</translation>
+    </message>
+    <message>
+        <location filename="qhimduploaddialog.cpp" line="79"/>
+        <source>please wait while uploading %1 track(s)</source>
+        <translation>Por favor, espere mientras se sube(n) %1 pista(s)</translation>
+    </message>
+    <message>
+        <location filename="qhimduploaddialog.cpp" line="135"/>
+        <source>upload aborted by the user</source>
+        <translation>Subida abortada por el usuario</translation>
+    </message>
+</context>
+</TS>
