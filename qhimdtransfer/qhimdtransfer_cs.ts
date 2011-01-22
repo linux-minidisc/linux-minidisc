55918de5ce0dec5ebd7f70ff83deb71db05ac90e
Martin Vican <vican@cesnet.cz>
2011-01-22 14:21:16 +0100

    Added Czech translation.

---
+++ qhimdtransfer/qhimdtransfer_cs.ts
@@ -0,0 +1,372 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="cs_CZ">
+<context>
+    <name>QHiMDAboutDialog</name>
+    <message>
+        <location filename="qhimdaboutdialog.ui" line="35"/>
+        <source>About QHiMDTransfer</source>
+        <translation>O programu QHiMDTransfer</translation>
+    </message>
+    <message>
+        <location filename="qhimdaboutdialog.ui" line="70"/>
+        <source>About</source>
+        <translation>O programu</translation>
+    </message>
+    <message>
+        <location filename="qhimdaboutdialog.ui" line="82"/>
+        <source>&lt;h3&gt;QHiMDTransfer, Version 0.0.1&lt;/h3&gt;
+        &lt;p&gt;&lt;p&gt;
+        &lt;p&gt;A free music transfer software for MiniDisc walkman.&lt;/p&gt;
+        &lt;p&gt;&lt;p&gt;
+        &lt;p&gt;Brought to you by the linux-minidisc project:&lt;/p&gt;
+        &lt;p&gt;&lt;p&gt;
+        &lt;p&gt;https://wiki.physik.fu-berlin.de/linux-minidisc&lt;/p&gt;
+        &lt;p&gt;&lt;p&gt;
+        &lt;p&gt;This software is covered by the GNU GPL v2 license. See the
+         file LICENSE in the program folder for more information.&lt;/p&gt;</source>
+        <translatorcomment>LICENSE =&gt; LICENCE (if would be also czech variant)</translatorcomment>
+        <translation>&lt;h3&gt;QHiMDTransfer, verze 0.0.1&lt;/h3&gt;
+        &lt;p&gt;&lt;p&gt;
+        &lt;p&gt;Svobodný software na přenos hudby pro MiniDisc walkman.&lt;/p&gt;
+        &lt;p&gt;&lt;p&gt;
+        &lt;p&gt;Poskytuje vám ho projekt linux-minidisc:&lt;/p&gt;
+        &lt;p&gt;&lt;p&gt;
+        &lt;p&gt;https://wiki.physik.fu-berlin.de/linux-minidisc&lt;/p&gt;
+        &lt;p&gt;&lt;p&gt;
+        &lt;p&gt;Tento software je zveřejněn pod licencí GNU GPL verze 2.
+         Více informací je uvedeno v souboru LICENSE ve složce programu.&lt;/p&gt;</translation>
+    </message>
+    <message>
+        <location filename="qhimdaboutdialog.ui" line="97"/>
+        <source>Contributors</source>
+        <translation>Přispěvatelé</translation>
+    </message>
+    <message>
+        <location filename="qhimdaboutdialog.ui" line="154"/>
+        <source>&amp;Close</source>
+        <translation>&amp;Zavřít</translation>
+    </message>
+</context>
+<context>
+    <name>QHiMDFormatDialog</name>
+    <message>
+        <location filename="qhimdformatdialog.ui" line="14"/>
+        <source>Format Medium</source>
+        <translation>Formátovat médium</translation>
+    </message>
+    <message>
+        <location filename="qhimdformatdialog.ui" line="41"/>
+        <source>Really format MiniDisc (all audio-tracks and data will be lost) ?</source>
+        <translation>Opravdu si přejete naformátovat MiniDisc (všechny skladby i data budou ztraceny)?</translation>
+    </message>
+</context>
+<context>
+    <name>QHiMDMainWindow</name>
+    <message>
+        <location filename="qhimdmainwindow.cpp" line="22"/>
+        <source>Error opening file for MP3 output</source>
+        <translation>Chyba otevírání výstupního souboru MP3</translation>
+    </message>
+    <message>
+        <location filename="qhimdmainwindow.cpp" line="27"/>
+        <location filename="qhimdmainwindow.cpp" line="91"/>
+        <location filename="qhimdmainwindow.cpp" line="151"/>
+        <source>Error opening track: </source>
+        <translation>Chyba při otevírání skladby:</translation>
+    </message>
+    <message>
+        <location filename="qhimdmainwindow.cpp" line="33"/>
+        <location filename="qhimdmainwindow.cpp" line="103"/>
+        <location filename="qhimdmainwindow.cpp" line="171"/>
+        <source>Error writing audio data</source>
+        <translation>Chyba zápisu audio dat</translation>
+    </message>
+    <message>
+        <location filename="qhimdmainwindow.cpp" line="40"/>
+        <source>upload aborted by the user</source>
+        <translation>čtení zrušeno uřivatelem</translation>
+    </message>
+    <message>
+        <location filename="qhimdmainwindow.cpp" line="46"/>
+        <source>Error reading audio data: </source>
+        <translation>Chyba čtení audio dat:</translation>
+    </message>
+    <message>
+        <location filename="qhimdmainwindow.cpp" line="86"/>
+        <source>Error opening file for ATRAC output</source>
+        <translation>Chyba otevírání výstupního souboru ATRAC</translation>
+    </message>
+    <message>
+        <location filename="qhimdmainwindow.cpp" line="96"/>
+        <source>Error writing header</source>
+        <translation>Chyba zápisu hlavičky</translation>
+    </message>
+    <message>
+        <location filename="qhimdmainwindow.cpp" line="146"/>
+        <source>Error opening file for WAV output</source>
+        <translation>Chyba otevírání výstupního souboru WAV</translation>
+    </message>
+    <message>
+        <location filename="qhimdmainwindow.cpp" line="303"/>
+        <source>Error opening HiMD data. Make sure you chose the proper root directory of your HiMD-Walkman.
+</source>
+        <translation>Chyba otevírání HiMD dat. Ujistěte se, zda je vybraná správná složka připojeného HiMD walkmanu.</translation>
+    </message>
+    <message>
+        <location filename="qhimdmainwindow.cpp" line="338"/>
+        <source>Track %1</source>
+        <translation>Skladba %1</translation>
+    </message>
+    <message>
+        <location filename="qhimdmainwindow.cpp" line="365"/>
+        <source>upload disabled because of DRM encryption</source>
+        <translation>Čtení je zakázáno z důvodu ochrany DRM</translation>
+    </message>
+    <message>
+        <location filename="qhimdmainwindow.cpp" line="415"/>
+        <source>Select MP3s for download</source>
+        <translation>Vyberte MP3 ke stažení</translation>
+    </message>
+    <message>
+        <location filename="qhimdmainwindow.cpp" line="425"/>
+        <source>Select directory for Upload</source>
+        <translation>Vyberte složku pro načtené skladby</translation>
+    </message>
+    <message>
+        <location filename="qhimdmainwindow.cpp" line="457"/>
+        <source>Select directory of HiMD Medium</source>
+        <translation>Vyberte složku HiMD média</translation>
+    </message>
+    <message>
+        <location filename="qhimdmainwindow.cpp" line="536"/>
+        <source>(disconnected)</source>
+        <translation>(odpojeno)</translation>
+    </message>
+</context>
+<context>
+    <name>QHiMDMainWindowClass</name>
+    <message>
+        <location filename="qhimdmainwindow.ui" line="14"/>
+        <source>QHiMDTransfer</source>
+        <translation>QHiMDTransfer</translation>
+    </message>
+    <message>
+        <location filename="qhimdmainwindow.ui" line="38"/>
+        <source>HiMD path</source>
+        <translation>HiMD složka</translation>
+    </message>
+    <message>
+        <location filename="qhimdmainwindow.ui" line="45"/>
+        <source>(disconnected)</source>
+        <translation>(odpojeno)</translation>
+    </message>
+    <message>
+        <location filename="qhimdmainwindow.ui" line="138"/>
+        <source>local directory</source>
+        <translation>místní složka</translation>
+    </message>
+    <message>
+        <location filename="qhimdmainwindow.ui" line="176"/>
+        <source>&amp;File</source>
+        <translation>&amp;Soubor</translation>
+    </message>
+    <message>
+        <location filename="qhimdmainwindow.ui" line="192"/>
+        <source>&amp;?</source>
+        <translation>&amp;?</translation>
+    </message>
+    <message>
+        <location filename="qhimdmainwindow.ui" line="229"/>
+        <source>&amp;Download tracks onto MD</source>
+        <translation>&amp;Zápis skladeb na MD</translation>
+    </message>
+    <message>
+        <location filename="qhimdmainwindow.ui" line="238"/>
+        <source>&amp;Upload tracks from MD</source>
+        <translation>Č&amp;tení skladeb z MD</translation>
+    </message>
+    <message>
+        <location filename="qhimdmainwindow.ui" line="247"/>
+        <source>Re&amp;name</source>
+        <translation>&amp;Přejmenovat</translation>
+    </message>
+    <message>
+        <location filename="qhimdmainwindow.ui" line="250"/>
+        <source>Rename selected</source>
+        <translation>Přejmenovat vybrané</translation>
+    </message>
+    <message>
+        <location filename="qhimdmainwindow.ui" line="259"/>
+        <source>D&amp;elete selected</source>
+        <translation>&amp;Vymazat vybrané</translation>
+    </message>
+    <message>
+        <location filename="qhimdmainwindow.ui" line="268"/>
+        <source>&amp;Help</source>
+        <translation>&amp;Nápověda</translation>
+    </message>
+    <message>
+        <location filename="qhimdmainwindow.ui" line="277"/>
+        <source>&amp;About</source>
+        <translation>&amp;O aplikaci</translation>
+    </message>
+    <message>
+        <location filename="qhimdmainwindow.ui" line="286"/>
+        <source>&amp;Format MD</source>
+        <translation>&amp;Formátovat MD</translation>
+    </message>
+    <message>
+        <location filename="qhimdmainwindow.ui" line="289"/>
+        <source>Format MD</source>
+        <translation>Formátování MD</translation>
+    </message>
+    <message>
+        <location filename="qhimdmainwindow.ui" line="298"/>
+        <source>&amp;Quit</source>
+        <translation>&amp;Konec</translation>
+    </message>
+    <message>
+        <location filename="qhimdmainwindow.ui" line="307"/>
+        <source>&amp;Connect</source>
+        <translation>Přip&amp;ojit</translation>
+    </message>
+</context>
+<context>
+    <name>QHiMDTracksModel</name>
+    <message>
+        <location filename="qhimdmodel.cpp" line="149"/>
+        <source>Nr.</source>
+        <translation>Č.</translation>
+    </message>
+    <message>
+        <location filename="qhimdmodel.cpp" line="151"/>
+        <source>Title</source>
+        <translation>Název</translation>
+    </message>
+    <message>
+        <location filename="qhimdmodel.cpp" line="153"/>
+        <source>Artist</source>
+        <translation>Interpret</translation>
+    </message>
+    <message>
+        <location filename="qhimdmodel.cpp" line="155"/>
+        <source>Album</source>
+        <translation>Album</translation>
+    </message>
+    <message>
+        <location filename="qhimdmodel.cpp" line="157"/>
+        <source>Length</source>
+        <translation>Délka</translation>
+    </message>
+    <message>
+        <location filename="qhimdmodel.cpp" line="159"/>
+        <source>Format</source>
+        <translation>Formát</translation>
+    </message>
+    <message>
+        <location filename="qhimdmodel.cpp" line="161"/>
+        <source>Uploadable</source>
+        <translation>Lze přenést</translation>
+    </message>
+</context>
+<context>
+    <name>QHiMDUploadDialog</name>
+    <message>
+        <location filename="qhimduploaddialog.ui" line="14"/>
+        <source>Track upload - QHiMDTransfer</source>
+        <translation>Čtení skladeb – QHiMDTransfer</translation>
+    </message>
+    <message>
+        <location filename="qhimduploaddialog.ui" line="55"/>
+        <source>please wait while uploading XX track(s)</source>
+        <translation>Čekejte na přečtení XX skladeb</translation>
+    </message>
+    <message>
+        <location filename="qhimduploaddialog.ui" line="69"/>
+        <source>overall progress</source>
+        <translation>Celkový postup</translation>
+    </message>
+    <message>
+        <location filename="qhimduploaddialog.ui" line="93"/>
+        <source>current track: trknum - filename</source>
+        <translatorcomment>trknum = číslo skladby; filename = soubor</translatorcomment>
+        <translation>Aktuální skladba: trknum - filename</translation>
+    </message>
+    <message>
+        <location filename="qhimduploaddialog.ui" line="108"/>
+        <source>xx track(s) successfully uploaded</source>
+        <translation>Úspěšně načteno skladeb: xx</translation>
+    </message>
+    <message>
+        <location filename="qhimduploaddialog.ui" line="115"/>
+        <source>xx track(s) could not be uploaded</source>
+        <translation>Počet skladeb, které nelze přenést: xx</translation>
+    </message>
+    <message>
+        <location filename="qhimduploaddialog.ui" line="129"/>
+        <source>Show Details</source>
+        <translation>Podrobnosti</translation>
+    </message>
+    <message>
+        <location filename="qhimduploaddialog.ui" line="142"/>
+        <source>Cancel</source>
+        <translation>Zrušit</translation>
+    </message>
+    <message>
+        <location filename="qhimduploaddialog.ui" line="149"/>
+        <source>Close</source>
+        <translation>Zavřít</translation>
+    </message>
+    <message>
+        <location filename="qhimduploaddialog.ui" line="186"/>
+        <source>Tracknr.</source>
+        <translation>Číslo skladby</translation>
+    </message>
+    <message>
+        <location filename="qhimduploaddialog.ui" line="191"/>
+        <source>Error message</source>
+        <translation>Chybová zpráva</translation>
+    </message>
+    <message>
+        <location filename="qhimduploaddialog.cpp" line="10"/>
+        <source>%1 track(s) could not be uploaded</source>
+        <translation>Počet skladeb, které nemohly být přeneseny: %1</translation>
+    </message>
+    <message>
+        <location filename="qhimduploaddialog.cpp" line="15"/>
+        <source>Track %1</source>
+        <translation>Skladba %1</translation>
+    </message>
+    <message>
+        <location filename="qhimduploaddialog.cpp" line="28"/>
+        <source>%1 track(s) successfully uploaded</source>
+        <translation>Počet úspěšně přečtených skladeb: %1</translation>
+    </message>
+    <message>
+        <location filename="qhimduploaddialog.cpp" line="33"/>
+        <source>upload finished</source>
+        <translation>Čtení dokončeno</translation>
+    </message>
+    <message>
+        <location filename="qhimduploaddialog.cpp" line="48"/>
+        <source>current track: %1 - %2</source>
+        <translation>Aktuální skladba: %1 - %2</translation>
+    </message>
+    <message>
+        <location filename="qhimduploaddialog.cpp" line="74"/>
+        <source>no tracks selected</source>
+        <translation>Nebyla vybraná žádná skladby</translation>
+    </message>
+    <message>
+        <location filename="qhimduploaddialog.cpp" line="79"/>
+        <source>please wait while uploading %1 track(s)</source>
+        <translation>Čekejte na přečtení %1 skladeb</translation>
+    </message>
+    <message>
+        <location filename="qhimduploaddialog.cpp" line="135"/>
+        <source>upload aborted by the user</source>
+        <translation>Čtení zrušeno uživatelem</translation>
+    </message>
+</context>
+</TS>
