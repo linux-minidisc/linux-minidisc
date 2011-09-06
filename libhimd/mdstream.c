bcfe8d8b19d4e00cffc8587e44a7587200d55d39
Adrian Glaubitz <glaubitz@physik.fu-berlin.de>
2011-09-06 21:44:16 +0200

    Change licenses for libraries 'libnetmd' and 'libhimd' from GPLv2 to LGPLv2.1 to allow linking the libraries to non-GPL software.

--- libhimd/mdstream.c
+++ libhimd/mdstream.c
@@ -1,3 +1,28 @@
+/*
+ * mdstream.c
+ *
+ * This file is part of libhimd, a library for accessing Sony HiMD devices.
+ *
+ * Copyright (C) 2009-2011 Michael Karcher
+ * Copyright (C) 2011 Mårten Cassel
+ * Copyright (C) 2011 Thomas Arp
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ */
+
 #include <string.h>
 #include <stdlib.h>
 #include <errno.h>
--- libnetmd/secure.c
+++ libnetmd/secure.c
@@ -1,6 +1,28 @@
 /*
-  Copyright 2004 Bertrik Sikken (bertrik@zonnet.nl)
+ * secure.c
+ *
+ * This file is part of libnetmd, a library for accessing Sony NetMD devices.
+ *
+ * Copyright (C) 2004 Bertrik Sikken
+ * Copyright (C) 2011 Alexander Sulfrian
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ */
 
+/*
   Set of NetMD commands that start with the sequence
   0x00,0x18,0x00,0x08,0x00,0x46,0xf0,0x03,0x01,0x03
 
