aaef12719ff3f4625b5c773ef71bc3c06a0c7361
John Paul Adrian Glaubitz <glaubitz@physik.fu-berlin.de>
2012-11-23 15:16:59 +0100

    Update get_version.sh to be independent from pwd path.

--- get_version.sh
+++ get_version.sh
@@ -2,11 +2,12 @@
 # get_version.sh - simple script to determine version number
 
 GIT_VERSION="$(git describe --always --long 2>/dev/null)"
+SRCDIR="$( cd "$( dirname "$0" )" && pwd )"
 
 if [ -n "$GIT_VERSION" ] ; then
     echo $GIT_VERSION
-elif [ -f VERSION ] ; then
-    echo $(< VERSION)
+elif [ -f ${SRCDIR}/VERSION ] ; then
+    echo $(< ${SRCDIR}/VERSION)
 else
     echo "NO_VERSION"
 fi
