b4d1e807be76a9a2160bb97992d510c78ada6408
Thomas Perl <m@thp.io>
2022-05-11 06:05:19 +0200

    QHiMDTransfer: Enable logging of libnetmd via '-v'

--- qhimdtransfer/main.cpp
+++ qhimdtransfer/main.cpp
@@ -10,9 +10,17 @@
 #define SHARE_FROM_BIN "/../share/qhimdtransfer"
 #endif
 
+#include "libnetmd.h"
+
 int main(int argc, char *argv[])
 {
     int status;
+
+    for (int i=1; i<argc; ++i) {
+        if (strcmp(argv[i], "-v") == 0) {
+            netmd_set_log_level(NETMD_LOG_ALL);
+        }
+    }
 
     QApplication a(argc, argv);
     QTranslator trans;
